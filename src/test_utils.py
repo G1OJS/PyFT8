88ed7e0209161d7f43448a501f645b73240d3d89
G1OJS <140509227+G1OJS@users.noreply.github.com>
2025-10-16 16:39:08 +0100

    enhanced wsjt-compare output

--- src/test_utils.py
+++ src/test_utils.py
@@ -1,8 +1,6 @@
 import time
 
 def wsjtx_tailer():
-    wsjtx_lines = []
-    wsjtx_prevlines=[]
     cycle = ''
     def follow(path):
         with open(path, "r") as f:
@@ -18,33 +16,45 @@ def wsjtx_tailer():
         if(cycle != line[7:13]):
             cycle = line[7:13]
             with open('last wsjtx output.txt', 'w') as f:
-                for l in wsjtx_prevlines:
-                    f.write(f"{l}\n")
-            wsjtx_prevlines = wsjtx_lines
-            wsjtx_lines=[]
-        wsjtx_lines.append(line)
+                f.write(f"{line}\n")
+        else:
+            with open('last wsjtx output.txt', 'a') as f:
+                f.write(f"{line}\n")            
+
 
 def wsjtx_compare(lines):
     loc_patterns, wsj_patterns = set(), set()
-    
-    for myline in lines:
-        loc_patterns.add(myline['msg'].replace(' ',''))
-        
+    matched_msgs, unmatched_msgs = [],[]
     with open('last wsjtx output.txt', 'r') as f:
         wsjt = f.readlines()
     for wsjtline in wsjt:
         l = wsjtline[48:]
         wsj_pattern = l.replace(' ','').replace('\n','')
         wsj_patterns.add(wsj_pattern)
-        if(wsj_pattern in loc_patterns):
-            l += " ***"
-     #   print(l.replace('\n',''))
+    for myline in lines:
+        l = myline['msg']
+        loc_pattern = l.replace(' ','')
+        if(loc_pattern in wsj_patterns):
+            matched_msgs.append(l)
+        else:
+            unmatched_msgs.append(l)
+        loc_patterns.add(loc_pattern)
+
+    print("wsjt-x:")
+    for l in wsjt:
+        print(l.replace('\n',''))
+    print("Matched with wsjt-x:")
+    for l in matched_msgs:
+        print(l)
+    print("Not matched with wsjt-x:")
+    for l in unmatched_msgs:
+        print(l)
 
     matches = loc_patterns.intersection(wsj_patterns)
     loc_only = loc_patterns.difference(wsj_patterns)
-    A = len(loc_patterns)
-    B = len(wsj_patterns)
-    AB = len(matches)
-    AnotB = len(loc_only)
-    pc = AB/B if B>0 else 0
-    print(f"wsjt:{B} matched: {AB} ({pc:.1%}) unmatched: {AnotB}")
+    loc = len(loc_patterns)
+    wsj = len(wsj_patterns)
+    both = len(matches)
+    loc_only = len(loc_only)
+    pc = loc/wsj if wsj>0 else 0
+    print(f"wsjt:{wsj} above: {loc} ({pc:.1%}) matched: {both} unmatched: {loc_only}")
