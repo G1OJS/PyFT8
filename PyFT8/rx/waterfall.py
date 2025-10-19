4e05e4b84b922f06e26a11644daee0ecafb25b29
G1OJS <140509227+G1OJS@users.noreply.github.com>
2025-10-19 14:24:28 +0100

    fix plotting?

--- PyFT8/rx/waterfall.py
+++ PyFT8/rx/waterfall.py
@@ -4,63 +4,65 @@ import matplotlib.patches as patches
 from matplotlib.colors import LogNorm
 
 class Waterfall:
-    def __init__(self, wf, t0=0, t1=15, f0=100, f1=3500):
+    def __init__(self, specbuff, hops_persymb, fbins_pertone, costas, t0=0, t1=15, f0=100, f1=3500):
+        self.f0, self.f1, self.t0, self.t1 = t0, t1, f0, f1
+        self.costas = costas
+        self.hops_persymb = hops_persymb
+        self.fbins_pertone = fbins_pertone
+        self.specbuff = specbuff
         self.fig, self.ax = plt.subplots(figsize=(10, 3))
-        self.im = self.ax.imshow(wf.power ,aspect='auto',origin='lower',
-                        extent=[wf.freqs[0], wf.freqs[-1], wf.times[0], wf.times[-1]],cmap='inferno',interpolation='none', norm=LogNorm())
+        dt = self.specbuff.length_secs / (self.specbuff.nHops - 1)
+        df = self.specbuff.width_Hz / (self.specbuff.nFreqs - 1)
+        self.extent = [self.specbuff.freqs[0] - 0.5 * df, self.specbuff.freqs[-1] - 0.5 * df,
+                       self.specbuff.times[0] - 0.5 * dt,  self.specbuff.times[-1] - 0.5 * dt]
+        self.im = self.ax.imshow(self.specbuff.power, aspect='auto',origin='lower',
+                        extent=self.extent,cmap='inferno',interpolation='none', norm=LogNorm())
         self.ax.set_xlabel('Frequency (Hz)')
         self.ax.set_ylabel('Time (s)')
-        self.ax.set_title('FT8 waterfall')
         self.ax.set_xlim(f0, f1)
         self.ax.set_ylim(t0, t1)
         self.candidate_plots=[]
-        #plt.pause(0.5)
 
-    def update(self, wf, title = "FT8 Waterfall", candidates = None, show_n_candidates = 0):
-        self.im.set_data(wf.power)
-        #vmax = wf.power.max()
-        #dBrange = 50
-        #vmin = vmax * 10**(-dBrange/20)  
-        #self.im.set_norm(LogNorm(vmin=vmin, vmax=vmax))
+    def update(self, title = "FT8 Waterfall", candidates = None, show_n_candidates = 0):
+        self.im.set_data(np.abs(self.specbuff.complex))
+        print(self.specbuff.complex.shape)
         self.im.autoscale()
         [p.remove() for p in reversed(self.ax.patches)]
         if(candidates):
             for i, c in enumerate(candidates):
-                rect = patches.Rectangle((c.freq-0.5*c.hz_pertone, c.dt-0.5*c.symbol_secs),
-                                         8*c.hz_pertone, c.num_symbols * c.symbol_secs,
-                                         linewidth=2, edgecolor='w', facecolor='none')
+                t0_c = self.specbuff.times[c.tbin_idx] 
+                f0_c = self.specbuff.freqs[c.fbin_idx] 
+                dt_c = self.specbuff.times[c.tbin_idx + self.hops_persymb * c.num_symbols] - t0_c
+                df_c = self.specbuff.freqs[c.fbin_idx + self.fbins_pertone * c.tones_persymb] - f0_c
+                rect = patches.Rectangle(  (f0_c, t0_c), df_c, dt_c,
+                                            linewidth=2, edgecolor='w', facecolor='none')
                 self.ax.add_patch(rect)
-                if(i<show_n_candidates): self.candidate_plots.append(show_candidate(wf,c))
+                if(i<show_n_candidates):
+                    self.candidate_plots.append(self.show_zoom(c, t0_c, t0_c+dt_c, f0_c, f0_c+df_c))
+                    
         self.ax.set_title(title)
         self.fig.canvas.draw_idle()
         self.fig.canvas.flush_events()
         plt.pause(0.5)
 
-def show_candidate(wf, candidate):
-    costas = [3, 1, 4, 0, 6, 5, 2]
-    t0_idx = candidate.tbin_idx
-    f0_idx = candidate.fbin_idx
-    t1_idx = t0_idx + candidate.num_symbols * candidate.hops_persymb
-    f1_idx = f0_idx + 8 * candidate.fbins_pertone
-    fig, axs = plt.subplots(1,2)
-    cpwr = wf.power[t0_idx:t1_idx, f0_idx:f1_idx]
-    cphs = np.angle(wf.complex[t0_idx:t1_idx, f0_idx:f1_idx])
-    axs[0].imshow(cpwr,aspect='auto',origin='lower',cmap='inferno',interpolation='none',norm=LogNorm(),
-              extent = [-0.5, 7.5, -0.5,-0.5+ candidate.num_symbols * candidate.hops_persymb])
-    axs[1].imshow(cphs,aspect='auto',origin='lower',cmap='inferno',interpolation='none',norm=LogNorm(),
-              extent = [-0.5, 7.5, -0.5,-0.5+ candidate.num_symbols * candidate.hops_persymb])
-    for iax in [0,1]:
-        for i, tone in enumerate(costas):
-            for j in [0,36,72]:
-                rect = patches.Rectangle((tone -0.5, (i+j-0.5)*candidate.hops_persymb),
-                                      1, candidate.hops_persymb, linewidth=2, facecolor='none',
-                                     edgecolor='black' )
-                axs[iax].add_patch(rect)
-            
-        axs[iax].set_xlim(-0.5, 7.5)
-        axs[iax].set_ylim(-0.5, -0.5+ candidate.num_symbols * candidate.hops_persymb)
-        axs[iax].set_xlabel('Tone')
-        axs[iax].set_ylabel('Hop')
-    axs[0].set_title(f"FT8 candidate @ {candidate.freq} costas = {candidate.costas_score}")
-    plt.pause(0.1)
-    return fig
+    def show_zoom(self, c, t0, t1, f0, f1):
+        fig, axs = plt.subplots(2,1,figsize=(2, 5))
+        im = axs[0].imshow(self.specbuff.power, aspect='auto',origin='lower',
+                        extent=self.extent,cmap='inferno',interpolation='none', norm=LogNorm())
+        for symb_idx, tone_idx in enumerate(self.costas):
+            for symb_copy_idx in [symb_idx, symb_idx+36, symb_idx+72]:
+                t0_sync = self.specbuff.times[c.tbin_idx + symb_copy_idx * self.hops_persymb]
+                f0_sync = self.specbuff.freqs[c.fbin_idx + tone_idx * self.fbins_pertone]
+                dt_sync = self.hops_persymb * self.specbuff.length_secs  / self.specbuff.nHops
+                df_sync = self.fbins_pertone * self.specbuff.width_Hz  / self.specbuff.nFreqs
+                rect = patches.Rectangle((f0_sync, t0_sync),  df_sync, dt_sync, edgecolor='black', facecolor= 'none' )
+                axs[0].add_patch(rect)
+        axs[0].set_title(f"{f0:.1f}Hz")
+        axs[0].set_xlim(f0, f1)
+        axs[0].set_ylim(t0, t1)
+
+        im2 = axs[1].imshow(c.power_grid, aspect='auto',origin='lower',
+                        extent=[-0.5, -0.5 + c.tones_persymb, -0.5, -0.5 + c.num_symbols] ,cmap='inferno',interpolation='none', norm=LogNorm())
+        
+        plt.pause(0.5)
+        return plt
