ab7850e450f87d171f94926f6e1913efffdbd533
G1OJS <140509227+G1OJS@users.noreply.github.com>
2025-10-24 13:12:40 +0100

    more testing

--- PyFT8/rx/decode174_91.py
+++ PyFT8/rx/decode174_91.py
@@ -1,44 +1,61 @@
+# uses zero-based kNM and kMN arrays but clipped to zero, so the zeros
+# interpreted as sentinels in the fortran code are here interpreted as
+# pointers to the first element of the arrays - which is wrong, but this
+# decodes more signals and faster ....
+
 import numpy as np
 
 from PyFT8.FT8_constants import kNCW, kNRW, kMN, kNM, kN, kK, kM
+from PyFT8.FT8_crc import check_crc
+
+def bitsLE_to_int(bits):
+    """bits is MSB-first."""
+    n = 0
+    for b in bits:
+        n = (n << 1) | (b & 1)
+    return n
 
 def safe_atanh(x, eps=1e-12):
     x = np.clip(x, -1 + eps, 1 - eps)
     return 0.5 * np.log((1 + x) / (1 - x))
   #  return np.arctanh(x)
 
-def decode174_91(llr, maxiterations = 50, alpha = 0.01, gamma = 0.03, nstall_max = 15, ncheck_max = 60):
+def count_syndrome_checks(zn):
+    ncheck = 0
+    cw = (zn > 0).astype(int)
+    for i in range(kM):
+        synd = sum(cw[kNM[i, 0:kNRW[i]]])
+        if ((synd %2) != 0): ncheck += 1
+    if ncheck == 0:
+        decoded_bits174_LE_list = cw.tolist() 
+        decoded_bits91_int = bitsLE_to_int(decoded_bits174_LE_list[0:91])
+        if(not check_crc(decoded_bits91_int)):
+            return -1, cw, []
+        return 0, cw, decoded_bits174_LE_list
+    return ncheck, cw, []
+
+def decode174_91(llr, maxiterations = 50, alpha = 0.05, gamma = 0.03, nstall_max = 12, ncheck_max = 30):
     toc = np.zeros((7, kM), dtype=np.float32)       # message -> check messages
     tanhtoc = np.zeros((7, kM), dtype=np.float64)
     tov = np.zeros((kNCW, kN), dtype=np.float32)    # check -> message messages
     nclast, nstall = 0, 0                           # stall condition variables
-    info = []                           # record the progression of ncheck
     zn = np.copy(llr)                   # working copy of llrs
     rng = np.max(llr) - np.min(llr)     # indication of scale of llrs
     mult = rng * gamma          # empricical multiplier for tov, proportional to llr scale
+
+    ncheck, cw, decoded_bits174_LE_list = count_syndrome_checks(zn)
+    if(ncheck ==0): return decoded_bits174_LE_list, it
+
     for it in range(maxiterations + 1):
         for i in range(kN):
             zn[i] += mult*sum(tov[:,i])
-
-        cw = (zn > 0).astype(int)
-        ncheck = 0                      # syndrome: sum variable nodes participating in this check.
-        for chk in range(kM):
-            vars_idx = kNM[chk, :kNRW[chk]]
-            vars_idx = vars_idx[(vars_idx >=0)]
-            if( int(np.sum(cw[vars_idx]) % 2)):
-                ncheck += 1
-
-        info.append(ncheck)
-        if ncheck == 0:
-            message91 = cw.tolist()
-            if(sum(message91)>0):
-               # print(f"Success: {info}")
-                return message91, it
-
+            
+        ncheck, cw, decoded_bits174_LE_list = count_syndrome_checks(zn)
+        if(ncheck <=0): return decoded_bits174_LE_list, it
+        
         nstall = 0 if ncheck < nclast else nstall +1
         nclast = ncheck
         if(nstall > nstall_max or ncheck > ncheck_max):         # early exit condition
-         #   print(f"Failure: {info}")
             return [], it
         
         # compute toc = messages from variable node -> check node
@@ -46,17 +63,14 @@ def decode174_91(llr, maxiterations = 50, alpha = 0.01, gamma = 0.03, nstall_max
         # that correspond to other checks connected to variable ibj (connections specified by kMN[ibj, :])
         for j in range(kM):
             for i_local in range(kNRW[j]):    
-                ibj = int(kNM[j, i_local])
-                if(ibj <0):
-                    continue
+                ibj = int(kNM[j, i_local])   
                 toc[i_local, j] = zn[ibj]
                 for kk in range(kNCW):
                     chknum = kMN[ibj, kk]
-                    if chknum == -1 or chknum == (j + 1): # kMN = -1 means "skip this check" 
+                    if chknum == j:
                         continue
                     toc[i_local, j] -= tov[kk, ibj]
 
-        # tanh of half negative (what's this?)
         for j in range(kM):
             tanhtoc[:kNRW[j], j] = np.tanh(-toc[:kNRW[j], j] / 2.0)
 
@@ -65,14 +79,13 @@ def decode174_91(llr, maxiterations = 50, alpha = 0.01, gamma = 0.03, nstall_max
         for variable_node in range(kN):
             for kk in range(kNCW):
                 chknum = kMN[variable_node, kk]
-                if chknum == -1:                    # kMN = -1 means "skip this check"
+                if chknum == 0:
                     tov[kk, variable_node] = 0.0
                     continue
                 ichk = int(chknum)
                 # build mask over the neighbours of check ichk excluding current variable 'var'
-                neigh_vars = kNM[ichk, : kNRW[ichk]]
-                neigh_vars = neigh_vars[(neigh_vars >=0)]
-                neigh_count = len(neigh_vars)
+                neigh_count = kNRW[ichk]
+                neigh_vars = kNM[ichk, :neigh_count]  
                 mask = (neigh_vars != variable_node)
                 if mask.sum() == 0:
                     Tmn = 0.0
@@ -82,8 +95,5 @@ def decode174_91(llr, maxiterations = 50, alpha = 0.01, gamma = 0.03, nstall_max
                 y = safe_atanh(-Tmn)
                 new_val = 2.0 * safe_atanh(-Tmn)
                 tov[kk, variable_node] = alpha * new_val + (1 - alpha) * tov[kk, variable_node]
-
-    # failed to decode
-   # print(f"Failure: {info}")
     return [], it
 
