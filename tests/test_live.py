c46b4513199de4a420b44b79c1a25cb2066c9cbe
G1OJS <140509227+G1OJS@users.noreply.github.com>
2025-10-29 08:32:09 +0000

    Update test_live.py

--- tests/test_live.py
+++ tests/test_live.py
@@ -12,7 +12,8 @@ import PyFT8.timers as timers
 
 SAMPLE_RATE = 12000
 CYCLE = 15.0
-SHORT_CYCLE = 14.5
+SHORT_CYCLE = 14.8      # allow time for file handover etc between cycles
+START_CAPTURE = -0.2    # offset start of audio capture from cycle start
 FRAMES_PER_CYCLE = int(SAMPLE_RATE * SHORT_CYCLE)
 BRIDGE_FILE = 'audio.wav'
 FLAG_FILE = 'audio.txt'
@@ -44,10 +45,7 @@ def wsjtx_compare(wsjtx_file, PyFT8_file):
     global lw_tot, lp_tot, best_snr_alltime
 
     import sys
-    try:
-        color = sys.stdout.shell
-    except AttributeError:
-        raise RuntimeError("Use IDLE")
+    color = sys.stdout.shell
 
     with open(wsjtx_file, 'r') as f:
         wsjt_lines = f.readlines()
@@ -64,21 +62,24 @@ def wsjtx_compare(wsjtx_file, PyFT8_file):
 
     best_snr = 50
     for i, l in enumerate(wsjt_lines):
-        color.write(f"{l}", "STRING" if(wsjt_patterns[i] in PyFT8_patterns) else "KEYWORD")
+        if(wsjt_patterns[i] in PyFT8_patterns):
+            color.write(f" BOTH: {l}", "STRING")
+        else:
+            color.write(f"WSJTX: {l}", "KEYWORD")
         if(wsjt_patterns[i] in PyFT8_patterns):
             snr = int(l[34:37])
             if (snr<best_snr): best_snr = snr
 
     for i, l in enumerate(PyFT8_lines):
         if(PyFT8_patterns[i] not in wsjt_patterns): 
-            color.write(f"{l}", "COMMENT")
+            color.write(f"PyFT8: {l}", "COMMENT")
 
     lw, lp = len(wsjt_lines), len(PyFT8_lines)
     lw_tot += lw
     lp_tot += lp
     if(best_snr < best_snr_alltime): best_snr_alltime = best_snr    
-    print(f"This Cycle: WSJT-X:{lw} PyFT8:{lp} -> {lp/(1e-12+lw):.0%} best snr = {best_snr}")
-    print(f"All cycles: WSJT-X:{lw_tot} PyFT8:{lp_tot} -> {lp_tot/(1e-12+lw_tot):.0%} best snr = {best_snr_alltime}")
+    print(f"This Cycle: WSJTX:{lw} PyFT8:{lp} -> {lp/(1e-12+lw):.0%} best snr = {best_snr}")
+    print(f"All cycles: WSJTX:{lw_tot} PyFT8:{lp_tot} -> {lp_tot/(1e-12+lw_tot):.0%} best snr = {best_snr_alltime}")
 
 def dumpwav(filename, data):
     wavefile = wave.open(filename, 'wb')
@@ -93,7 +94,7 @@ def audioloop():
     while True:
         timers.timedLog("Audio capture waiting for cycle start")
         t_elapsed, t_remaining = timers.time_in_cycle()
-        time.sleep(t_remaining-0.25)
+        time.sleep(t_remaining + START_CAPTURE)
         stream = pya.open(format=pyaudio.paInt16, channels = 1, rate=SAMPLE_RATE,
                       input=True, input_device_index = 1,
                       frames_per_buffer=FRAMES_PER_CYCLE)
@@ -135,8 +136,6 @@ while True:
     os.remove(FLAG_FILE)
     timers.timedLog(f"Start to Load audio from {BRIDGE_FILE}")
     demod.spectrum.feed_audio(audio)
-    timers.timedLog("Start to Show spectrum")
-    wf.update_main()
     timers.timedLog("Start to Find candidates")
     candidates = demod.find_candidates(100,3300, topN=500)
     timers.timedLog(f"Found {len(candidates)} candidates")
@@ -146,13 +145,13 @@ while True:
     timers.timedLog("Start to sync candidates")
     candidates = demod.sync_candidates(candidates, topN=30)
     timers.timedLog(f"Synced {len(candidates)} candidates")
-    timers.timedLog("Start to Show candidates")
-    wf.update_main(candidates=candidates)
-    #wf.show_zoom(candidates=candidates[:5])
     timers.timedLog("Start to demodulate candidates")
     decodes = demod.demodulate(candidates, cyclestart_str = cycle_str)
     t_decodes_finished, _ = timers.time_in_cycle()
     timers.timedLog(f"Decodes: {len(decodes)}")
+    timers.timedLog("Start to Show spectrum and candidates")
+    wf.update_main(candidates=candidates)
+    #wf.show_zoom(candidates=candidates[:5])
         
     with open(PyFT8_file, 'a') as f:
         for l in decodes:
