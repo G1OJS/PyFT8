6dc6a96554c162dea41ff467ed85a82ead47ee78
G1OJS <140509227+G1OJS@users.noreply.github.com>
2025-11-08 18:27:37 +0000

    Update cycle_decoder.py

--- PyFT8/rx/cycle_decoder.py
+++ PyFT8/rx/cycle_decoder.py
@@ -36,12 +36,13 @@ def get_decodes(onStart=None, onDecode=None, onOccupancy=None, onFinished=None):
     cyclestart_str = timers.cyclestart_str(0)
     demod.spectrum.load_audio(audio_in)
 
-
     # decode the Rx freq first
+    timers.timedLog("[Cycle decoder] Get Rx freq decode")
     f0_idx = int(np.searchsorted(demod.spectrum.freqs, config.rxfreq))
     rx_freq_candidate = Candidate(demod.sigspec, demod.spectrum, 0, f0_idx, -50)
     demod.sync_candidate(rx_freq_candidate)
     decode = demod.demodulate_candidate(rx_freq_candidate, cyclestart_str = cyclestart_str)
+    timers.timedLog("[Cycle decoder] Rx freq decoding done")
     if(onStart):
         onStart()
     if(decode):
@@ -60,6 +61,7 @@ def get_decodes(onStart=None, onDecode=None, onOccupancy=None, onFinished=None):
         decode = demod.demodulate_candidate(c, cyclestart_str)
         if(onDecode):
             onDecode(decode)
+    timers.timedLog("[Cycle decoder] all decoding done")
     if(onFinished):
         onFinished()
 
