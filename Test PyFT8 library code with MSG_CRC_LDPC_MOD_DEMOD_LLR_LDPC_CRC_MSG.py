aed76e0513d12fde384d9ec137b02da574837b4c
G1OJS <140509227+G1OJS@users.noreply.github.com>
2025-12-13 20:15:27 +0000

    initial

---
+++ Test PyFT8 library code with MSG_CRC_LDPC_MOD_DEMOD_LLR_LDPC_CRC_MSG.py
@@ -0,0 +1,94 @@
+import numpy as np
+from PyFT8.waterfall import Waterfall
+import PyFT8.timers as timers
+from PyFT8.comms_hub import config
+from PyFT8.sigspecs import FT8
+from PyFT8.cycle_manager import Cycle_manager
+
+from PyFT8.FT8_encoder import pack_ft8_c28, pack_ft8_g15, encode_bits77
+import PyFT8.audio as audio
+
+wav_file = "Local_gen_test.wav"
+
+c28a = pack_ft8_c28("VK1ABC")
+c28b = pack_ft8_c28("VK3JPK")
+g15, ir = pack_ft8_g15("QF22")
+i3 = 1
+n3 = 0
+
+print(f"Expected:  VK1ABC 1110000111111100010100110101")
+print(f"Generated: VK1ABC {c28a:028b}")
+print(f"Expected:  VK3JPK 1110001000000111101000011110")
+print(f"Generated: VK3JPK {c28b:028b}")
+print(f"Expected:  QF22 111001010001010")
+print(f"Generated: QF22 {g15:015b}")
+
+bits77 = (c28a<<28+1+2+15+3) | (c28b<<2+15+3)|(0<<15+3)|(g15<< 3)|(i3)
+print("bits expected / bits encoded")
+print("11100001111111000101001101010111000100000011110100001111000111001010001010001")
+print(f"{bits77:077b}")
+
+symbols, bits174_int, bits91_int, bits14_int, bits83_int = encode_bits77(bits77)
+print("CRC expected / produced:")
+print("00111100110010")
+print(f"{bits14_int:014b}")
+print("Bits91:")
+print("1110000111111100010100110101011100010000001111010000111100011100101000101000100111100110010")
+print(f"{bits91_int:091b}")
+print("LDPC Parity Bits expected / produced:")
+print("01101010111110101110000011111111010100101110011011100110010000000000011100010000001")
+print(f"{bits83_int:083b}")
+print("Bits174:")
+print("111000011111110001010011010101110001000000111101000011110001110010100010100010011110011001001101010111110101110000011111111010100101110011011100110010000000000011100010000001")
+print(f"{bits174_int:0174b}")
+
+print(f"Payload symbols  expected:   {'7027413236410076024143535324211637464027735642254300025301'}")
+print(f"Channel symbols modulated:   {''.join([str(s) for s in symbols])}")
+
+symbols_framed = [-10]*7
+symbols_framed.extend(symbols)
+symbols_framed.extend([-10]*7)
+print(f"({len(symbols)} symbols)")
+audio_out = audio.AudioOut()
+audio_data = audio_out.create_ft8_wave(symbols_framed, f_base = 3000, amplitude = 0.1, added_noise = -20)
+audio_out.write_to_wave_file(audio_data, wav_file)
+
+global decoded_candidates
+decoded_candidates = []
+first = True
+def onDecode(c):
+    global first
+    global cycle_manager
+    if(first):
+        heads = ['End_cyc+', 'Rx call', 'Tx call', 'GrRp', 'SyncScr', 'LLR_sd', 'snr', 't0_idx', 'f0_idx', 't0', 'f0',  'iters']
+        print(''.join([f"{t:>8} " for t in heads]))
+        first = False
+    dd = c.decode_dict
+    t_decode = timers.tnow() % 15 - 15
+    vals = [f"{t_decode:8.2f}", dd['call_a'], dd['call_b'], dd['grid_rpt'],
+            f"{dd['sync_score']:>5.2f}", f"{dd['llr_sd']:5.2f}", f"{dd['snr']:5.0f}",
+            dd['t0_idx'], dd['f0_idx'], f"{dd['dt']:8.2f}s", f"{dd['freq']:8.2f}Hz", dd['n_its']]
+
+    print(''.join([f"{t:>8} " for t in vals]))
+    decoded_candidates.append(c)
+
+start_load = timers.tnow()
+cycle_manager = Cycle_manager(FT8, onDecode, onOccupancy = None, audio_in_wav = wav_file, 
+                          max_iters = 10, max_stall = 8, max_ncheck = 28, 
+                          sync_score_thresh = 2, max_cycles = 1,
+                            thread_PyFT8_decode_manager = True, return_candidate = True)
+
+while cycle_manager.running:
+    timers.sleep(0.5)
+    
+print(f"DONE.")
+
+print("bits expected / bits decoded")
+print("11100001111111000101001101010111000100000011110100001111000111001010001010001")
+if(decoded_candidates):
+    for c in decoded_candidates:
+        print(''.join(str(int(b)) for b in c.payload_bits[:77]))
+
+wf = Waterfall(cycle_manager.spectrum)
+wf.update_main(candidates=decoded_candidates)
+wf.show_zoom(candidates=decoded_candidates)
---
+++ timings_plotter.py
@@ -0,0 +1,105 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
+
+df = pd.read_csv("timings.log", na_values=["None", " None"])
+
+fig, axs = plt.subplots(2,1,figsize=(14, 7))
+
+ax = axs[0]
+def vbar(ax, x, t_start, t_end, color, lw=2, alpha=0.8, minbar = 0.05):
+    if(t_end < t_start + minbar):
+        t_end = t_start + minbar /2
+        t_start = t_start - minbar /2
+    ax.plot([x, x], [t_start, t_end], color=color, linewidth=lw, alpha=alpha)
+
+pc_to_ldpc = []
+pc_from_ldpc = []
+n_cands = 0
+n_to_ldpc =0
+n_from_ldpc = 0
+for _, row in df.iterrows():
+    vbar(ax, row["id"], row["sync_returned"], row["demap_requested"], color = "tab:blue", lw=1, alpha = 0.2 )
+    vbar(ax, row["id"], row["demap_requested"], row["demap_returned"], color = "tab:orange", lw=2.5, alpha = 0.4 )
+    col, alpha = ('tab:green', 1) if(not pd.isnull(row["message_decoded"])) else ('tab:red',1)
+    vbar(ax, row["id"], row["ldpc_requested"], row["ldpc_returned"], color = col, lw=2.5, alpha = alpha)
+    if(not pd.isnull(row["demap_returned"])): n_cands +=1
+    if(not pd.isnull(row["ldpc_requested"])): n_to_ldpc +=1
+    pc_to_ldpc.append(100*n_to_ldpc / n_cands)
+    if(not pd.isnull(row["ldpc_returned"])): n_from_ldpc +=1
+    pc_from_ldpc.append(100*n_from_ldpc / n_cands)
+
+ax.set_title("Candidate Timing with Pipeline Stage Durations")
+ax.set_xlabel("Candidate ID")
+ax.set_ylabel("Time within cycle (s)")
+ax.set_ylim(0,20)
+#ax.set_xlim(0,1200)
+
+from matplotlib.lines import Line2D
+legend_els = [
+    Line2D([0], [0], color="tab:blue", lw=1, alpha = 0.2 ),
+    Line2D([0], [0], color="tab:orange", lw=2.5 , alpha = 0.4 ),
+    Line2D([0], [0], color="tab:red", lw=2.5, alpha = 1  ),
+    Line2D([0], [0], color="tab:green", lw=2.5, alpha = 1 )
+]
+ax.legend(legend_els,['await rx','demap','ldpc fail','ldpc success'], loc = 'upper left')
+
+ax2 = ax.twinx()
+ax2.set_ylabel("Proportion of demapped sent/received ldpc")
+ax2.plot(df['id'],pc_to_ldpc, c='tab:blue', label = '% to ldpc')
+ax2.plot(df['id'],pc_from_ldpc, c='tab:orange', label = '% from ldpc')
+ax2.legend(loc = 'upper right')
+ax2.set_ylim(0,100)
+
+events = []
+
+for _, r in df.iterrows():
+    cid = r["id"]
+
+    # Demap enqueue/dequeue
+    if not pd.isnull(r["demap_requested"]):
+        events.append((r["demap_requested"], "demap", +1, cid))
+    if not pd.isnull(r["demap_returned"]):
+        events.append((r["demap_returned"],  "demap", -1, cid))
+
+    # LDPC enqueue/dequeue
+    if not pd.isnull(r["ldpc_requested"]):
+        events.append((r["ldpc_requested"], "ldpc", +1, cid))
+    if not pd.isnull(r["ldpc_returned"]):
+        events.append((r["ldpc_returned"],  "ldpc", -1, cid))
+
+# sort all events by time
+events.sort(key=lambda e: e[0])
+
+times = []
+demap_qlen = []
+ldpc_qlen = []
+
+dq = 0
+lq = 0
+
+for t, stage, delta, cid in events:
+    if stage == "demap":
+        dq += delta
+    else:
+        lq += delta
+    times.append(t)
+    demap_qlen.append(dq)
+    ldpc_qlen.append(lq)
+
+ax_q = axs[1]
+ax_q.plot(times, demap_qlen, label="demap queue")
+ax_q.plot(times, ldpc_qlen, label="ldpc queue")
+ax_q.set_xlabel("Time within cycle (s)")
+ax_q.set_ylabel("Queue length")
+ax_q.legend()
+
+
+ax_q.grid(alpha=0.2)
+plt.tight_layout()
+plt.show()
+
+
+
+
