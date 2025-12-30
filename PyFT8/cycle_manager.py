ae3e08e256c1bf6db872c968cd528bbaace70fb2
G1OJS <140509227+G1OJS@users.noreply.github.com>
2025-12-30 21:50:38 +0000

    fix introduced bug of all offsets = 0.0

--- PyFT8/cycle_manager.py
+++ PyFT8/cycle_manager.py
@@ -17,12 +17,19 @@ eps = 1e-12
 
 ldpc_NRW = [7,6,6,6,7,6,7,6,6,7,6,6,7,7,6,6,6,7,6,7,6,7,6,6,6,7,6,6,6,7,6,6,6,6,7,6,6,6,7,7,6,6,6,6,7,7,6,6,6,6,7,6,6,6,7,6,6,6,6,7,6,6,6,7,6,6,6,7,7,6,6,7,6,6,6,6,6,6,6,7,6,6,6]
 ldpc_NM = -1 + np.array([ [4,5,6,7,8,6,5,9,10,11,12,13,8,14,15,1,16,17,11,45,8,18,19,20,2,21,22,16,23,19,20,14,3,19,7,12,13,24,25,20,21,35,14,4,1,26,52,7,23,26,2,27,18,6,28,9,22,3,31,12,5,2,15,10,23,11,29,30,10,22,28,28,1,17,51,21,16,3,9,15,18,25,17],[31,32,24,33,25,32,34,35,36,37,38,39,40,41,42,33,43,37,44,55,46,36,38,47,48,45,47,39,43,35,36,31,44,46,49,50,51,52,53,46,54,82,30,29,4,51,84,50,55,41,27,40,49,33,48,54,53,13,69,43,39,54,56,44,34,49,34,50,53,57,32,29,26,27,57,37,47,24,40,58,42,38,42],[59,60,61,62,63,64,65,66,67,67,68,69,70,71,59,72,73,74,75,64,71,76,77,70,74,78,58,62,79,59,63,79,80,81,58,61,64,76,69,65,77,133,83,68,52,56,110,81,67,77,41,56,55,85,70,63,68,48,133,66,75,86,87,82,71,88,87,60,66,85,72,84,45,89,98,73,76,30,90,60,79,65,75],[91,93,94,95,83,97,78,99,100,87,102,103,82,88,106,106,108,81,110,111,112,89,104,92,113,83,118,112,120,73,94,98,124,117,90,118,114,129,90,80,100,142,113,120,57,91,115,99,95,109,61,124,124,108,85,131,109,78,150,89,102,101,108,91,94,92,97,86,84,93,103,88,80,103,163,138,130,72,106,74,144,99,129],[92,115,122,96,93,126,98,139,107,101,105,149,104,102,123,107,141,109,121,130,119,113,116,138,128,117,127,134,131,110,136,132,127,135,100,119,118,148,101,120,140,171,125,134,86,122,145,132,172,141,62,125,141,116,105,147,121,95,155,97,136,135,119,111,127,142,147,137,112,140,132,117,128,116,165,152,137,104,134,111,146,122,170],[96,146,151,143,96,138,107,146,126,139,155,162,114,123,159,157,160,131,166,161,166,114,163,165,160,121,164,158,145,125,161,164,169,167,105,144,157,149,130,140,171,174,170,173,136,137,168,173,174,148,115,126,167,156,129,155,174,123,169,135,167,164,171,144,153,157,162,142,128,159,166,143,147,153,172,169,154,139,151,150,152,160,172],[153,0,0,0,148,0,154,0,0,158,0,0,145,156,0,0,0,154,0,173,0,143,0,0,0,151,0,0,0,161,0,0,0,0,168,0,0,0,156,170,0,0,0,0,152,168,0,0,0,0,133,0,0,0,158,0,0,0,0,159,0,0,0,149,0,0,0,162,165,0,0,150,0,0,0,0,0,0,0,163,0,0,0],])
+
 ldpc_check_vars = np.full((83, 7), -1, dtype=np.int16)
 ldpc_check_deg  = np.zeros(83, dtype=np.int8)
 for m in range(83):
     v = ldpc_NM[:ldpc_NRW[m], m]
     ldpc_check_vars[m, :len(v)] = v
     ldpc_check_deg[m] = len(v)
+
+synd_check_idxs=[]
+for m in range(83):
+    ichk = ldpc_NM[:ldpc_NRW[m], m]
+    ichk = ichk[(ichk >=0)]
+    synd_check_idxs.append(ichk)
 
 def safe_pc(x,y):
     return 100*x/y if y>0 else 0
@@ -39,12 +46,13 @@ class Candidate:
 
     def __init__(self):
         self.deduped = False
-        self.sync_started, self.sync_completed = None, None
-        self.demap_started, self.demap_completed = None, None
-        self.decode_started, self.decode_completed = None, None
+        self.sync_started, self.sync_completed_times = None, None
+        self.demap_started, self.demap_completed_times = None, None
+        self.decode_started, self.decode_completed_times = None, None
         self.info_str = ""
         self.decoded_stage = None
         self.msg = None
+        self.ncheck = 999
 
     def record_sync(self, spectrum, h0_idx, f0_idx, score):
         hps, bpt = spectrum.hops_persymb, spectrum.fbins_pertone
@@ -55,7 +63,7 @@ class Candidate:
         self.sync_score = score
         self.dt = self.h0_idx * spectrum.dt-0.7
         self.fHz = int((self.f0_idx + bpt // 2) * spectrum.df)
-        self.sync_completed = time.time()
+        self.sync_completed_times = time.time()
                                    
     def demap(self, spectrum):
         self.demap_started = time.time()
@@ -69,22 +77,26 @@ class Candidate:
         llr = np.column_stack((llr0, llr1, llr)).ravel()
         llr_sd = np.std(llr)
         self.llr = 3.8 * llr / llr_sd
-        llr_check = self.llr[ldpc_check_vars]
-        parity = (np.sum((llr_check > 0) & (ldpc_check_vars != -1), axis=1) & 1) 
-        self.ncheck =  np.sum(parity)
+        self.ncheck =  self.get_ncheck(self.llr)
         self.info_str = f"{self.ncheck};"
-        self.demap_completed = time.time()
+        self.demap_completed_times = time.time()
 
     def find_llr_offset(self, llr):
-        offsets = [0.1,0.3,0.5,0.7,0.9,1.1,1.5,2.0]
-        offsets = np.array(offsets + [-o for o in offsets])
+        off_pos = np.arange(0.1,2.0,0.1)
+        offsets = np.concatenate([off_pos, -off_pos])
         llrs_with_offsets = llr + offsets[:, None]
+        valid_mask = (ldpc_check_vars != -1)[None, :, :]
         llrs_for_check = llrs_with_offsets[:, ldpc_check_vars]
-        parity = (np.sum((llrs_for_check > 0) & (ldpc_check_vars != -1), axis=2) & 1)
+        bits = (llrs_for_check > 0)
+        parity = (np.sum(bits * valid_mask, axis=2) & 1)
         nchecks = np.sum(parity, axis=1)
         best_idx = np.argmin(nchecks)
         ncheck = nchecks[best_idx]
         return offsets[best_idx], ncheck
+
+    def get_ncheck(self, llr):
+        synd_checks = [ sum(1 for llr_bit in llr[synd_check_idxs[i]] if llr_bit > 0) %2 for i in range(83)]
+        return int(np.sum(synd_checks))
   
     def ldpc(self, llr, max_iters = 15):
         Lmn = np.zeros((83, 7), dtype=np.float32)        
@@ -102,9 +114,7 @@ class Candidate:
                 delta[v] += new - Lmn[m, :deg]
                 Lmn[m, :deg] = new
             llr += delta
-            llr_check = llr[ldpc_check_vars]
-            parity = (np.sum((llr_check > 0) & (ldpc_check_vars != -1), axis=1) & 1) 
-            ncheck = np.sum(parity)
+            ncheck = self.get_ncheck(llr)
             info_str += f"{ncheck},"
             if(ncheck == 0): break
         return llr, info_str, ncheck
@@ -118,11 +128,11 @@ class Candidate:
 
         if(self.ncheck > 0):
             llr_orig = self.llr.copy()
-            threshold = 26
+            threshold = 28
             
             if(self.ncheck > threshold):
                 offset, nchk = self.find_llr_offset(self.llr)
-                if(nchk < self.ncheck - 1):
+                if(nchk < self.ncheck):
                     self.ncheck = nchk
                     self.llr += offset
                 else:
@@ -133,17 +143,17 @@ class Candidate:
             if(self.ncheck == 0 and not self.decoded_stage):
                 self.decoded_stage = 'L1'
 
-            if(self.ncheck > 0 and (self.ncheck < 5 or offset != 0)):
+            if(self.ncheck > 0 and self.ncheck < 10):
                 if(offset !=0):
                     offset = 0
                     self.llr = llr_orig.copy()
-                self.info_str = self.info_str + f" {offset:5.2f}:"        
+                self.info_str = self.info_str + f" {offset:5.2f}:"
                 self.llr, self.ldpc_info, self.ncheck = self.ldpc(self.llr, max_iters = 8)
                 self.info_str = self.info_str + self.ldpc_info
             if(self.ncheck == 0 and not self.decoded_stage):
                 self.decoded_stage = 'L2'
 
-        self.decode_completed = time.time()
+        self.decode_completed_times = time.time()
         
         if(self.ncheck > 0):
             self.decoded_stage = 'F'
@@ -232,11 +242,10 @@ class Spectrum:
 
 class Cycle_manager():
     def __init__(self, sigspec, onSuccessfulDecode, onOccupancy, audio_in_wav = None,
-                 ncheck_max = 36, ncheck_max_fast = 28, max_cycles = 5000, 
+                 ncheck_max_fast = 28, max_cycles = 5000, 
                  input_device_keywords = None, output_device_keywords = None, verbose = False):
         self.running = True
         self.verbose = verbose
-        self.ncheck_max = ncheck_max
         self.ncheck_max_fast = ncheck_max_fast
         self.audio_in_wav = audio_in_wav
         self.input_device_idx = find_device(input_device_keywords)
@@ -254,6 +263,10 @@ class Cycle_manager():
         self.sigspec = sigspec
         self.spectrum = Spectrum(sigspec)
         self.audio_started = False
+        self.sync_completed_times = [0]
+        self.demap_completed_times = [0]
+        self.demap_valid_ncheck_times = [0]
+        self.decode_completed_times = [0]
 
         threading.Thread(target=self.manage_cycle, daemon=True).start()
         delay = self.sigspec.cycle_seconds - self.spectrum.cycle_time()
@@ -276,21 +289,15 @@ class Cycle_manager():
     def print_stats(self):
         if(self.verbose): 
             def earliest_and_latest(arr): return f"first {np.min(arr)%15 :5.2f}, last {np.max(arr)%15 :5.2f}" if arr else ''
-            with self.cands_lock:
-                sync_completed = [c.sync_completed for c in self.cands_list if c.sync_completed]
-                demap_completed = [c.demap_completed for c in self.demapped_cands]
-                demap_valid_ncheck = [c.demap_completed for c in self.demapped_cands if c.ncheck <= self.ncheck_max]
-                decode_completed = [c.decode_completed for c in self.cands_list if c.decode_completed]
-            self.tlog(f"[Cycle manager] sync_completed:   {len(sync_completed)} ({earliest_and_latest(sync_completed)})")
-            self.tlog(f"[Cycle manager] demap_completed: {len(demap_completed)} ({earliest_and_latest(demap_completed)})")
-            self.tlog(f"[Cycle manager] ncheck_valid: {len(demap_valid_ncheck)} ({earliest_and_latest(demap_valid_ncheck)})")
-            self.tlog(f"[Cycle manager] decode_completed:  {len(decode_completed)} ({earliest_and_latest(decode_completed)})")
+            self.tlog(f"[Cycle manager] sync_completed:   {len(self.sync_completed_times)} ({earliest_and_latest(self.sync_completed_times)})")
+            self.tlog(f"[Cycle manager] demap_completed: {len(self.demap_completed_times)} ({earliest_and_latest(self.demap_completed_times)})")
+            self.tlog(f"[Cycle manager] decode_completed:  {len(self.decode_completed_times)} ({earliest_and_latest(self.decode_completed_times)})")
             counts = Counter(c.decoded_stage for c in self.cands_list)
             items = ", ".join(f"{k}:{v}" for k,v in counts.items())
             self.tlog(f"Decode types = {items}")
         
     def manage_cycle(self):
-        cycle_searched = True
+        cycle_searched = False
         cycle_counter = 0
         cycle_time_prev = 0
         self.demapped_cands = []
@@ -306,11 +313,15 @@ class Cycle_manager():
                 if(cycle_counter > self.max_cycles):
                     self.running = False
                     break
+                self.stats_printed = False
                 cycle_searched = False
                 self.check_for_tx()
                 self.spectrum.pgrid_fine_ptr = 0
-                self.stats_printed = False
                 if not self.audio_started: self.start_audio()
+
+            if(time.time() %15 > 3 and not self.stats_printed):
+                self.stats_printed = True
+                self.print_stats()
 
             if (self.spectrum.pgrid_fine_ptr > self.spectrum.h_search and not cycle_searched):
                 cycle_searched = True
@@ -319,8 +330,10 @@ class Cycle_manager():
                 if(self.verbose): self.tlog(f"[Cycle manager] Spectrum searched -> {len(new_cands)} candidates")
                 if(self.onOccupancy): self.onOccupancy(self.spectrum.occupancy, self.spectrum.df)
                 with self.cands_lock:
-                    self.cands_list = new_cands
-
+                    self.cands_list = new_cands + [c for c in self.cands_list
+                                                   if (time.time() - c.sync_completed_times) < 15 and not c.decode_completed_times]
+                    self.sync_completed_times = [c.sync_completed_times for c in self.cands_list if c.sync_completed_times]
+            
             if(self.spectrum.pgrid_fine_ptr >= self.spectrum.h_demap):
                 with self.cands_lock:
                     to_demap = [c for c in self.cands_list
@@ -333,17 +346,16 @@ class Cycle_manager():
                         c.decode(self.duplicate_filter, self.onSuccessfulDecode)
 
             with self.cands_lock:
-                self.demapped_cands = [c for c in self.cands_list if c.demap_completed]
-            to_decode =  [c for c in self.demapped_cands if c.ncheck <= self.ncheck_max and not c.decode_started]
+                self.demapped_cands = [c for c in self.cands_list if c.demap_completed_times]
+                self.demap_completed_times = [c.demap_completed_times for c in self.demapped_cands]
+                
+            to_decode =  [c for c in self.demapped_cands if not c.decode_started]
             if(len(self.demapped_cands) < len(self.cands_list)):
                 to_decode = [c for c in to_decode if c.ncheck <= self.ncheck_max_fast]
             if(to_decode):
                 c = to_decode[np.argmin([c.ncheck for c in to_decode])]
                 c.decode(self.duplicate_filter, self.onSuccessfulDecode)
-
-            if(self.spectrum.cycle_time() > self.sigspec.cycle_seconds - 0.25 and not self.stats_printed):
-                self.stats_printed = True
-                self.print_stats()
+                self.decode_completed_times = [c.decode_completed_times for c in self.cands_list if c.decode_completed_times]
 
     def check_for_tx(self):
         from .FT8_encoder import pack_message
