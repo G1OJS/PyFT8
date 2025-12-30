2d086c8b63f6faba6c44043d7085b423e9cb6f52
G1OJS <140509227+G1OJS@users.noreply.github.com>
2025-12-30 09:57:12 +0000

    tidy / simplify

--- PyFT8/cycle_manager.py
+++ PyFT8/cycle_manager.py
@@ -3,7 +3,6 @@ from types import SimpleNamespace
 import numpy as np
 import time
 from .audio import find_device, AudioIn
-from .decode174_91_v7_0 import LDPC174_91
 from .FT8_unpack import FT8_unpack
 from PyFT8.FT8_crc import check_crc
 import pyaudio
@@ -11,8 +10,6 @@ import queue
 import wave
 import os
 eps = 1e-12
-
-ldpc = LDPC174_91()
 
 def safe_pc(x,y):
     return 100*x/y if y>0 else 0
@@ -28,27 +25,20 @@ class StageProps:
     def __init__(self):
         self.started_time   = None
         self.completed_time = None
-        self.success        = False
         self.result         = None      
         self.metrics        = None
-
     def start(self):
         self.started_time = time.time()
-
-    def complete(self, *, success=False, result=None, metrics=None):
+    def complete(self, *,  result=None, metrics=None):
         self.completed_time = time.time()
-        self.success = success
         self.result = result
         self.metrics = metrics        
-
     @property
     def has_started(self):
         return self.started_time is not None
-
     @property
     def has_completed(self):
         return self.completed_time is not None
-
     @property
     def is_in_progress(self):
         return self.has_started and not self.has_completed
@@ -60,6 +50,46 @@ class Pipeline:
         self.decode = StageProps()
         self.unpack = StageProps()
 
+class LDPC174_91:
+    def __init__(self):
+
+        self.kNRW = [7,6,6,6,7,6,7,6,6,7,6,6,7,7,6,6,6,7,6,7,6,7,6,6,6,7,6,6,6,7,6,6,6,6,7,6,6,6,7,7,6,6,6,6,7,7,6,6,6,6,7,6,6,6,7,6,6,6,6,7,6,6,6,7,6,6,6,7,7,6,6,7,6,6,6,6,6,6,6,7,6,6,6]
+        self.kNM = -1 + np.array([ [4,5,6,7,8,6,5,9,10,11,12,13,8,14,15,1,16,17,11,45,8,18,19,20,2,21,22,16,23,19,20,14,3,19,7,12,13,24,25,20,21,35,14,4,1,26,52,7,23,26,2,27,18,6,28,9,22,3,31,12,5,2,15,10,23,11,29,30,10,22,28,28,1,17,51,21,16,3,9,15,18,25,17],[31,32,24,33,25,32,34,35,36,37,38,39,40,41,42,33,43,37,44,55,46,36,38,47,48,45,47,39,43,35,36,31,44,46,49,50,51,52,53,46,54,82,30,29,4,51,84,50,55,41,27,40,49,33,48,54,53,13,69,43,39,54,56,44,34,49,34,50,53,57,32,29,26,27,57,37,47,24,40,58,42,38,42],[59,60,61,62,63,64,65,66,67,67,68,69,70,71,59,72,73,74,75,64,71,76,77,70,74,78,58,62,79,59,63,79,80,81,58,61,64,76,69,65,77,133,83,68,52,56,110,81,67,77,41,56,55,85,70,63,68,48,133,66,75,86,87,82,71,88,87,60,66,85,72,84,45,89,98,73,76,30,90,60,79,65,75],[91,93,94,95,83,97,78,99,100,87,102,103,82,88,106,106,108,81,110,111,112,89,104,92,113,83,118,112,120,73,94,98,124,117,90,118,114,129,90,80,100,142,113,120,57,91,115,99,95,109,61,124,124,108,85,131,109,78,150,89,102,101,108,91,94,92,97,86,84,93,103,88,80,103,163,138,130,72,106,74,144,99,129],[92,115,122,96,93,126,98,139,107,101,105,149,104,102,123,107,141,109,121,130,119,113,116,138,128,117,127,134,131,110,136,132,127,135,100,119,118,148,101,120,140,171,125,134,86,122,145,132,172,141,62,125,141,116,105,147,121,95,155,97,136,135,119,111,127,142,147,137,112,140,132,117,128,116,165,152,137,104,134,111,146,122,170],[96,146,151,143,96,138,107,146,126,139,155,162,114,123,159,157,160,131,166,161,166,114,163,165,160,121,164,158,145,125,161,164,169,167,105,144,157,149,130,140,171,174,170,173,136,137,168,173,174,148,115,126,167,156,129,155,174,123,169,135,167,164,171,144,153,157,162,142,128,159,166,143,147,153,172,169,154,139,151,150,152,160,172],[153,0,0,0,148,0,154,0,0,158,0,0,145,156,0,0,0,154,0,173,0,143,0,0,0,151,0,0,0,161,0,0,0,0,168,0,0,0,156,170,0,0,0,0,152,168,0,0,0,0,133,0,0,0,158,0,0,0,0,159,0,0,0,149,0,0,0,162,165,0,0,150,0,0,0,0,0,0,0,163,0,0,0],])
+
+        self.check_vars = np.full((83, 7), -1, dtype=np.int16)
+        self.check_deg  = np.zeros(83, dtype=np.int8)
+        for m in range(83):
+            v = self.kNM[:self.kNRW[m], m]
+            self.check_vars[m, :len(v)] = v
+            self.check_deg[m] = len(v)
+  
+    def decode(self, llr, ncheck_hist, max_iters = 15):
+        Lmn = np.zeros((83, 7), dtype=np.float32)        
+        alpha = 1.18
+        for it in range(max_iters):
+            
+            llr_check = llr[self.check_vars]
+            valid = self.check_vars != -1
+            parity = (np.sum((llr_check > 0) & valid, axis=1) & 1) 
+            ncheck_hist.append(np.sum(parity))
+            if(ncheck_hist[-1] == 0): break
+            
+            delta = np.zeros_like(llr)
+            for m in range(83):
+                deg = self.check_deg[m]
+                v = self.check_vars[m, :deg]
+                Lnm = llr[v] - Lmn[m, :deg]
+                t = np.tanh(-Lnm)         
+                prod = np.prod(t) / t                       
+                new = prod / ((prod - alpha) * (alpha + prod))
+                delta[v] += new - Lmn[m, :deg]
+                Lmn[m, :deg] = new
+            llr += delta
+
+        return llr, ncheck_hist
+
+ldpc = LDPC174_91()
+
 class Candidate:
     next_id = 0
 
@@ -70,14 +100,11 @@ class Candidate:
         self.pipeline = Pipeline()
         self.deduped = False
 
-
-
     def record_sync(self, spectrum, h0_idx, f0_idx, score):
         hps, bpt = spectrum.hops_persymb, spectrum.fbins_pertone
         payload_hop_idxs  = [h0_idx + hps* s for s in spectrum.sigspec.payload_symb_idxs]   
         freq_idxs = [f0_idx + bpt // 2 + bpt * t for t in range(spectrum.sigspec.tones_persymb)]
         self.pipeline.sync.complete(
-            success=True,
             result=SimpleNamespace(score = score, f0_idx = f0_idx, h0_idx = h0_idx,
                                    payload_hop_idxs = payload_hop_idxs, freq_idxs = freq_idxs)
         )
@@ -99,7 +126,7 @@ class Candidate:
         valid = ldpc.check_vars != -1
         parity = (np.sum((llr_check > 0) & valid, axis=1) & 1) 
         self.ncheck_hist =  [np.sum(parity)]
-        self.pipeline.demap.complete(success=True,
+        self.pipeline.demap.complete(
             result  = SimpleNamespace(llr = llr, ncheck = self.ncheck_hist[-1]),
             metrics = SimpleNamespace(pgrid = pgrid, pmax=np.max(pgrid),llr_sd=llr_sd))
 
@@ -139,15 +166,15 @@ class Candidate:
                 if check_crc(bitsLE_to_int(decoded_bits[0:91]) ):
                     payload_bits = decoded_bits[:77]
                     
-        self.pipeline.decode.complete(success = any(payload_bits), result = SimpleNamespace(payload_bits = payload_bits),
+        self.pipeline.decode.complete(result = SimpleNamespace(payload_bits = payload_bits),
             metrics = SimpleNamespace(
                 ncheck_hist = self.ncheck_hist,
                 offset = offset,
                 info_str = f"{offset:5.2f},{','.join([str(h) for h in self.ncheck_hist])};"
             )
         )
-        if(self.pipeline.decode.success):
-            onSuccess(self, self.pipeline.decode.result.payload_bits)
+        if(any(payload_bits)):
+            onSuccess(self, payload_bits)
 
     @property
     def snr(self):
