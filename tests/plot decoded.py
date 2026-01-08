78e459f5e78f1923018e7ee1d69772aef2413ff0
G1OJS <140509227+G1OJS@users.noreply.github.com>
2026-01-08 09:02:18 +0000

    add result graph

--- tests/plot decoded.py
+++ tests/plot decoded.py
@@ -8,16 +8,15 @@ fix,ax = plt.subplots()
 
 n_its, nc = [],[]
 for l in lines:
-    decoded = l.find("L00")
+    decoded = l.find("CRC_passed")
     if(decoded>0):
         n_its.append(len([c for c in l[76:decoded] if c == "L"]))
-        nc.append(int(l[76:78]))
+        nc.append(int(l[68:70]))
 
-#ax.scatter(nc,n_its)
+ax.hist(nc, bins = range(50), cumulative = -1, density = True)
+ax.set_xlabel("Initial ncheck")
+ax.set_ylabel("Fraction of decodes (cumulative)")
 
-ax.hist(n_its, bins = range(25))
-ax.set_xlabel("Number of iterations")
-ax.set_ylabel("Number of decodes")
 
 plt.tight_layout()
 plt.show()
