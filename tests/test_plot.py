2428fa5e1a228bd76ad49b1efd6eae567b2add7e
G1OJS <140509227+G1OJS@users.noreply.github.com>
2026-01-11 18:16:26 +0000

    target osd etc based on llr quality

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -17,27 +17,33 @@ with open(f"compare_wsjtx.csv", "r") as f:
             if("L" in l): pl.append(n)
             if("Q" in l): po.append(n)
             if("B" in l): pb.append(n)
-        if("#" in l): fg.append(n)
-        if(not "#" in l): ft.append(n)
+        else:
+            if("#" in l): fg.append(n)
+            if(not "#" in l): ft.append(n)
 
 bins = [300 + 10*b for b in range(30)]
 
+np = len(p)
+nt = len(w)
+pc = f"{int(100*np/nt)}"
+
 for i in [0,1]:
-    axes[i].hist(w, bins = bins, label = "WSJTX", rwidth = 0.7, 
-            cumulative = i, color = '#D32F2F', alpha = 0.9, lw=0.5, edgecolor = "black")
-    axes[i].hist(p, bins = bins, label = "PyFT8", rwidth = 0.9, 
-            cumulative = i, color = '#388E3C', alpha = 0.9, lw=0.5, edgecolor = "black")
+    axes[i].hist(w, bins = bins, label = "WSJTX", rwidth = 0.4, 
+            cumulative = i, color = '#D32F2F', alpha = 1, lw=0.5, edgecolor = "black")
+    axes[i].hist(p, bins = bins, label = "PyFT8", rwidth = 1, 
+            cumulative = i, color = '#388E3C', alpha = 0.6, lw=0.5, edgecolor = "black")
     axes[i].hist(pi, bins = bins, label = "Immediate", rwidth=0.8,
-            cumulative = i, color = 'lime', alpha = 0.9, lw=0.5, edgecolor = "black")
+            cumulative = i, color = 'lime', alpha = 0.6, lw=0.5, edgecolor = "black")
     axes[i].hist(pb, bins = bins, label = "Incl. bit-flips", rwidth=0.5,
-            cumulative = i, color = 'orange', alpha = 0.9, lw=0.5, edgecolor = "black")
+            cumulative = i, color = 'orange', alpha = 0.6, lw=0.5, edgecolor = "black")
     axes[i].hist(po, bins = bins, label = "Incl. OSD", rwidth=0.3,
-            cumulative = i, color = 'yellow', alpha = 0.9, lw=0.5, edgecolor = "black")
-    axes[i].hist(po, bins = bins, label = "Timeouts", rwidth=0.1,
+            cumulative = i, color = 'yellow', alpha = 0.6, lw=0.5, edgecolor = "black")
+    axes[i].hist(ft, bins = bins, label = "Timeouts", rwidth=0.1,
             cumulative = i, color = 'black', alpha = 1.0, lw=0.2, edgecolor = "black")
 
     axes[i].set_xlabel("LLR Quality")
     axes[i].set_ylabel(f"Number of decodes{' (cumulative)' if i>0 else ''}")
+    fig.suptitle(f"PyFT8 detail vs WSJTX. {nt} decodes, {pc}% to PyFT8")
     axes[i].legend()
 
 
