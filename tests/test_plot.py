0885e5fce5c89875dccb19dce4eabbf693db89a9
G1OJS <140509227+G1OJS@users.noreply.github.com>
2026-01-14 11:08:09 +0000

    Ensure histogram mutually exclusive

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -1,53 +1,56 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-fig, ax = plt.subplots( figsize=(10,6))
-ws,py,pimm,pl,pba,po,ft = [],[],[],[],[],[],[]
-
 
 with open(f"compare_wsjtx.csv", "r") as f:
-    for lfull in f.readlines():
-        pdec = lfull[88] != " "
-        l = lfull[110:]
-        q = l[0:3]
-        q=float(q)
-        ws.append(q)
-        if(pdec):
-            py.append(q)
-            if('I00' in l): pimm.append(q)
-            if("L" in l): pl.append(q)
-            if("A" in l): pba.append(q)
-            if("O" in l): po.append(q)
-        else:
-            if(not "#" in l): ft.append(q)
+    lines=f.readlines()
 
+pycols = ['lime','green','orange','yellow','white']
+pylabs = ["Immediate","LDPC","LDPC & BitFlip","OSD","Timeouts"]
 bins = [325 + 5*b for b in range(50)]
 
-np = len(py)
-nt = len(ws)
-pc = f"{int(100*np/nt)}"
+py = [[],[],[],[],[]]
+ws = []
+pydecs = 0
+for lfull in lines:
+    l = lfull[110:]
+    q = l[0:3]
+    q=float(q)
+    if(lfull[88] != " "):
+        pydecs +=1
+        if('I00' in l):
+            py[0].append(q)
+        elif('O00' in l):
+            py[3].append(q)
+        elif('A' in l):
+            py[2].append(q)
+        elif('L' in l): py[1].append(q)
+            
+    if(not "#" in l): py[4].append(q)
+    ws.append(q)
 
+fig, ax = plt.subplots( figsize=(10,6))
 wsjtx = ax.hist(ws, bins = bins,  rwidth = 1.0, label = 'All',
         color = 'grey', alpha = 0.4, lw=1, edgecolor = 'grey')
 
-pyft8 = ax.hist([pimm,pl,pba,po,ft], bins = bins, rwidth = 0.5, 
-        stacked = True, alpha = 0.7, lw=1, edgecolor = 'grey',
-        color = ['lime','green','orange','yellow','white'])
+pyft8 = ax.hist(py, bins = bins, rwidth = 0.5, 
+        stacked = True, alpha = 0.7, lw=1, edgecolor = 'grey', color = pycols)
 
 legwidth = 0.18
 wsjtx_legend = ax.legend(handles=[wsjtx[2][0]], 
         loc='upper right', bbox_to_anchor=(1-legwidth,1, legwidth,0), mode='expand',
-        title = 'WSJT-X', title_fontproperties = {'weight':'bold', 'size':8}, alignment='left')
+        title = 'WSJT-X', title_fontproperties = {'weight':'bold', 'size':9}, alignment='left')
 ax.add_artist(wsjtx_legend)
-ax.legend(handles = pyft8[2],
-        labels = ["Immediate","LDPC","BitFlip","OSD","Timeouts"],
+ax.legend(handles = pyft8[2], labels = pylabs,
         loc = 'upper right', bbox_to_anchor=(1-legwidth,0.9, legwidth,0), mode='expand',
-        title = 'PyFT8', title_fontproperties = {'weight':'bold', 'size':8}, alignment='left')
-
+        title = 'PyFT8', title_fontproperties = {'weight':'bold', 'size':9}, alignment='left')
 
 ax.set_xlabel("Signal quality = sum of absolute values of log likelyhood ratios")
 ax.set_ylabel(f"Number of decodes")
-fig.suptitle(f"PyFT8 vs WSJTX. {nt} decodes, {pc}% to PyFT8")
+
+ntot = len(ws)
+py_pc = f"{int(100*pydecs/ntot)}"
+fig.suptitle(f"PyFT8 vs WSJTX. {ntot} decodes, {py_pc}% to PyFT8")
 
 plt.tight_layout()
 plt.show()
