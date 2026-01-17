1fa0031e4918e80f2d7e693f6b451c7208be02ba
G1OJS <140509227+G1OJS@users.noreply.github.com>
2026-01-17 12:28:09 +0000

    move ldpc to its own class and TU

--- PyFT8/cycle_manager.py
+++ PyFT8/cycle_manager.py
@@ -6,17 +6,17 @@ import time
 from .audio import find_device, AudioIn
 from .FT8_unpack import FT8_unpack
 from PyFT8.FT8_crc import check_crc_codeword_list
+from PyFT8.ldpc import LdpcDecoder
 from PyFT8.osd import osd_decode_minimal
 import pyaudio
 import queue
 import wave
 import os
 
+
 eps = 1e-12
 LLR_GEN =   {'abs_min':405, 'final_sd':3.3, 'clip':3.7}
 
-CHECK_VARS_6 = np.array([[4,31,59,92,114,145],[5,23,60,93,121,150],[6,32,61,94,95,142],[5,31,63,96,125,137],[8,34,65,98,138,145],[9,35,66,99,106,125],[11,37,67,101,104,154],[12,38,68,102,148,161],[14,41,58,105,122,158],[0,32,71,105,106,156],[15,42,72,107,140,159],[10,43,74,109,120,165],[7,45,70,111,118,165],[18,37,76,103,115,162],[19,46,69,91,137,164],[1,47,73,112,127,159],[21,46,57,117,126,163],[15,38,61,111,133,157],[22,42,78,119,130,144],[19,35,62,93,135,160],[13,30,78,97,131,163],[2,43,79,123,126,168],[18,45,80,116,134,166],[11,49,60,117,118,143],[12,50,63,113,117,156],[23,51,75,128,147,148],[20,53,76,99,139,170],[34,81,132,141,170,173],[13,29,82,112,124,169],[3,28,67,119,133,172],[51,83,109,114,144,167],[6,49,80,98,131,172],[22,54,66,94,171,173],[25,40,76,108,140,147],[26,39,55,123,124,125],[17,48,54,123,140,166],[5,32,84,107,115,155],[8,53,62,130,146,154],[21,52,67,108,120,173],[2,12,47,77,94,122],[30,68,132,149,154,168],[4,38,74,101,135,166],[1,53,85,100,134,163],[14,55,86,107,118,170],[22,33,70,93,126,152],[10,48,87,91,141,156],[28,33,86,96,146,161],[21,56,84,92,139,158],[27,31,71,102,131,165],[0,25,44,79,127,146],[16,26,88,102,115,152],[50,56,97,162,164,171],[20,36,72,137,151,168],[15,46,75,129,136,153],[2,23,29,71,103,138],[8,39,89,105,133,150],[17,41,78,143,145,151],[24,37,64,98,121,159],[16,41,74,128,169,171]], dtype = np.int16)
-CHECK_VARS_7 = np.array([[3,30,58,90,91,95,152],[7,24,62,82,92,95,147],[4,33,64,77,97,106,153],[10,36,66,86,100,138,157],[7,39,69,81,103,113,144],[13,40,70,87,101,122,155],[16,36,73,80,108,130,153],[44,54,63,110,129,160,172],[17,35,75,88,112,113,142],[20,44,77,82,116,120,150],[18,34,58,72,109,124,160],[6,48,57,89,99,104,167],[24,52,68,89,100,129,155],[19,45,64,79,119,139,169],[0,3,51,56,85,135,151],[25,50,55,90,121,136,167],[1,26,40,60,61,114,132],[27,47,69,84,104,128,157],[11,42,65,88,96,134,158],[9,43,81,90,110,143,148],[29,49,59,85,136,141,161],[9,52,65,83,111,127,164],[27,28,83,87,116,142,149],[14,57,59,73,110,149,162]], dtype = np.int16)
          
 generator_matrix_rows = ["8329ce11bf31eaf509f27fc",  "761c264e25c259335493132",  "dc265902fb277c6410a1bdc",  "1b3f417858cd2dd33ec7f62",  "09fda4fee04195fd034783a",  "077cccc11b8873ed5c3d48a",  "29b62afe3ca036f4fe1a9da",  "6054faf5f35d96d3b0c8c3e",  "e20798e4310eed27884ae90",  "775c9c08e80e26ddae56318",  "b0b811028c2bf997213487c",  "18a0c9231fc60adf5c5ea32",  "76471e8302a0721e01b12b8",  "ffbccb80ca8341fafb47b2e",  "66a72a158f9325a2bf67170",  "c4243689fe85b1c51363a18",  "0dff739414d1a1b34b1c270",  "15b48830636c8b99894972e",  "29a89c0d3de81d665489b0e",  "4f126f37fa51cbe61bd6b94",  "99c47239d0d97d3c84e0940",  "1919b75119765621bb4f1e8",  "09db12d731faee0b86df6b8",  "488fc33df43fbdeea4eafb4",  "827423ee40b675f756eb5fe",  "abe197c484cb74757144a9a",  "2b500e4bc0ec5a6d2bdbdd0",  "c474aa53d70218761669360",  "8eba1a13db3390bd6718cec",  "753844673a27782cc42012e",  "06ff83a145c37035a5c1268",  "3b37417858cc2dd33ec3f62",  "9a4a5a28ee17ca9c324842c",  "bc29f465309c977e89610a4",  "2663ae6ddf8b5ce2bb29488",  "46f231efe457034c1814418",  "3fb2ce85abe9b0c72e06fbe",  "de87481f282c153971a0a2e",  "fcd7ccf23c69fa99bba1412",  "f0261447e9490ca8e474cec",  "4410115818196f95cdd7012",  "088fc31df4bfbde2a4eafb4",  "b8fef1b6307729fb0a078c0",  "5afea7acccb77bbc9d99a90",  "49a7016ac653f65ecdc9076",  "1944d085be4e7da8d6cc7d0",  "251f62adc4032f0ee714002",  "56471f8702a0721e00b12b8",  "2b8e4923f2dd51e2d537fa0",  "6b550a40a66f4755de95c26",  "a18ad28d4e27fe92a4f6c84",  "10c2e586388cb82a3d80758",  "ef34a41817ee02133db2eb0",  "7e9c0c54325a9c15836e000",  "3693e572d1fde4cdf079e86",  "bfb2cec5abe1b0c72e07fbe",  "7ee18230c583cccc57d4b08",  "a066cb2fedafc9f52664126",  "bb23725abc47cc5f4cc4cd2",  "ded9dba3bee40c59b5609b4",  "d9a7016ac653e6decdc9036",  "9ad46aed5f707f280ab5fc4",  "e5921c77822587316d7d3c2",  "4f14da8242a8b86dca73352",  "8b8b507ad467d4441df770e",  "22831c9cf1169467ad04b68",  "213b838fe2ae54c38ee7180",  "5d926b6dd71f085181a4e12",  "66ab79d4b29ee6e69509e56",  "958148682d748a38dd68baa",  "b8ce020cf069c32a723ab14",  "f4331d6d461607e95752746",  "6da23ba424b9596133cf9c8",  "a636bcbc7b30c5fbeae67fe",  "5cb0d86a07df654a9089a20",  "f11f106848780fc9ecdd80a",  "1fbb5364fb8d2c9d730d5ba",  "fcb86bc70a50c9d02a5d034",  "a534433029eac15f322e34c",  "c989d9c7c3d3b8c55d75130",  "7bb38b2f0186d46643ae962",  "2644ebadeb44b9467d1f42c",  "608cc857594bfbb55d69600"]
 kGEN = np.array([int(row,16)>>1 for row in generator_matrix_rows])
@@ -105,7 +105,7 @@ class Candidate:
         self.llr0_quality = 0
         self.msg = None
         self.snr = -999
-        self.edges6, self.edges7 = None, None
+        self.ldpc = LdpcDecoder()
 
     def record_possible_syncs(self, spectrum, syncs):
         hps, bpt = spectrum.hops_persymb, spectrum.fbins_pertone
@@ -162,7 +162,7 @@ class Candidate:
         """
         
         self.llr0, self.llr0_quality, self.pgrid, self.snr = demap
-        self.ncheck0 = self.calc_ncheck(self.llr0)
+        self.ncheck0 = self.ldpc.calc_ncheck(self.llr0)
         self.llr = self.llr0.copy()
         self.ncheck = self.ncheck0
 
@@ -171,36 +171,12 @@ class Candidate:
         
         self.demap_completed = time.time()
 
-    def calc_ncheck(self, llr):
-        bits6 = llr[CHECK_VARS_6] > 0
-        self.parity6 = np.sum(bits6, axis=1) & 1
-        bits7 = llr[CHECK_VARS_7] > 0
-        self.parity7 = np.sum(bits7, axis=1) & 1
-        return int(np.sum(self.parity7) + np.sum(self.parity6))
-
     def record_state(self, actor_code, ncheck, final = False):
         self.ncheck = ncheck
         finalcode = "#" if final else ";"
         self.decode_path = self.decode_path + f"{actor_code}{ncheck:02d}{finalcode}"
         if(final):
             self.decode_completed = time.time()
-
-    def _pass_messages(self, idx, edges, delta):
-        if edges is None:
-            edges = np.zeros(idx.shape, dtype=np.float32)
-        v2c = self.llr[idx] - edges
-        t    = np.tanh(-v2c)
-        prod = np.prod(t, axis=1, keepdims=True) / t
-        new  = prod / ((prod - 1.18) * (1.18 + prod))
-        np.add.at(delta, idx, new - edges)
-        return new
-    
-    def do_ldpc_iteration(self):
-        delta = np.zeros_like(self.llr)
-        self.edges6 = self._pass_messages(CHECK_VARS_6, self.edges6, delta)
-        self.edges7 = self._pass_messages(CHECK_VARS_7, self.edges7, delta)
-        self.llr += delta
-        self.ncheck = self.calc_ncheck(self.llr)
 
     def flip_bits(self, width, nbits, keep_best = False):
 #        bad6 = CHECK_VARS_6[self.parity6.astype(bool)] 
@@ -215,7 +191,7 @@ class Candidate:
         for k in range(1, width + 1):
             for comb in itertools.combinations(range(len(idxs)), k):
                 self.llr[idxs[list(comb)]] *= -1
-                n = self.calc_ncheck(self.llr)
+                n = self.ldpc.calc_ncheck(self.llr)
                 if n < best['nc']:
                     best = {'llr':self.llr.copy(), 'nc':n}
                     if n == 0:
@@ -235,7 +211,8 @@ class Candidate:
 
         counter = 1
         if 35 > self.ncheck > 0 and not self.counters[counter] > 7:  
-            self.do_ldpc_iteration()
+            self.llr = self.ldpc.do_ldpc_iteration(self.llr)
+            self.ncheck = self.ldpc.calc_ncheck(self.llr)
             self.counters[counter] += 1
             return "L"
 
@@ -429,6 +406,6 @@ def check_G():
     print(c)
     cand.llr = np.where(c == 1, +1.0, -1.0)
     print(cand.llr)
-    assert cand.calc_ncheck(self.llr) == 0
+    assert cand.ldpc.calc_ncheck(self.llr) == 0
 
                     
