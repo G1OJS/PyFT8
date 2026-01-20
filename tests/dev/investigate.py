f1c7edffa0e8f4e80f639f740772b8f76fc4467a
G1OJS <140509227+G1OJS@users.noreply.github.com>
2026-01-20 14:23:13 +0000

    Update investigate.py

--- tests/dev/investigate.py
+++ tests/dev/investigate.py
@@ -26,12 +26,11 @@ def read_wav(wav_path):
 
 def get_spectrum(audio_samples):
     hops_per_cycle = int(15 * 6.25 * hps)
-    samples_per_hop = int(12000 / (6.25 * hps))
+    samples_per_hop = int(12000  / (6.25 * hps))
     print(samples_per_hop)
     fft_len = 1920 * bpt
     fft_out_len = fft_len//2 + 1
-    fft_window=np.kaiser(fft_len, 20)
-    zf = np.zeros((hops_per_cycle, fft_out_len), dtype = np.complex64)
+    fft_window=np.kaiser(fft_len, 7)
     pf = np.zeros((hops_per_cycle, fft_out_len), dtype = np.float32)
     for hop_idx in range(hops_per_cycle):
         x = np.zeros_like(fft_window)
@@ -40,35 +39,33 @@ def get_spectrum(audio_samples):
         x*=fft_window
         z = np.fft.rfft(x)
         p = z.real*z.real + z.imag*z.imag
-        zf[hop_idx, :] = z
         pf[hop_idx, :] = p
-    return zf, pf
+    return pf
 
-def get_tsyncs(pf, f0_idx):
-    pnorm = pf[:, f0_idx:f0_idx+8*bpt]
-    pnorm = pnorm / np.max(pnorm)
+def get_tsyncs(p):
     costas=[3,1,4,0,6,5,2]
-    csync = np.full((len(costas), 8*bpt), -1/(7*bpt), np.float32)
+    csync = np.full((len(costas)*hps, len(costas)*bpt), -1/(7*bpt), np.float32)
     for sym_idx, tone in enumerate(costas):
-        fbins = range(tone*bpt, (tone+1) * bpt)
-        csync[sym_idx, fbins] = 1.0
-        csync[sym_idx, len(costas)*bpt:] = 0
-
+        csync[sym_idx*hps:(sym_idx+1)*hps, tone*bpt:(tone+1)*bpt] = 1.0
     syncs = []
     block_off = 36 * hps
     hop_start_lattitude = int(2 * 6.25 * hps)
-    hop_idxs_Costas =  np.arange(len(costas)) * hps
+    hop_idxs_Costas =  np.arange(len(costas)* hps) 
+    f_idxs = np.arange(len(costas)* bpt) 
+    pnorm = p[:, f_idxs]
+    pnorm = pnorm / np.max(pnorm)
     for iBlock in [0,1]:
-        best = (0, f0_idx, -1e30)
+        best = (0, -1e30)
         for h0_idx in range(block_off * iBlock, block_off * iBlock + hop_start_lattitude):
-            sync_score = float(np.dot(pnorm[h0_idx + hop_idxs_Costas ,  :].ravel(), csync.ravel()))
-            test = (h0_idx - block_off * iBlock, f0_idx, sync_score)
-            if test[2] > best[2]:
+           # sync_score = float(np.dot(pnorm[h0_idx+hop_idxs_Costas,:].ravel(), csync.ravel()))
+            sync_score = np.sum(pnorm[h0_idx+hop_idxs_Costas,:] * csync)
+            test = (h0_idx - block_off * iBlock, sync_score)
+            if test[1] > best[1]:
                 best = test 
         syncs.append(best)
     return syncs
 
-def create_mask(msg):
+def create_symbols(msg):
     msg = msg.split(" ")
     c28a = pack_ft8_c28(msg[0]) 
     c28b = pack_ft8_c28(msg[1])
@@ -77,7 +74,7 @@ def create_mask(msg):
     n3 = 0
     bits77 = (c28a<<28+1+2+15+3) | (c28b<<2+15+3)|(0<<15+3)|(g15<< 3)|(i3)
     symbols, bits174_int, bits91_int, bits14_int, bits83_int = encode_bits77(bits77)
-    return np.array([[int(i/bpt)==tone for i in range(bpt * 8)] for tone in symbols for j in range(hps)])
+    return symbols
 
 def calc_dB(pwr, dBrange = 20, rel_to_max = False):
     thresh = np.max(pwr) * 10**(-dBrange/10)
@@ -86,31 +83,16 @@ def calc_dB(pwr, dBrange = 20, rel_to_max = False):
     if(rel_to_max):
         dB = dB - np.max(dB)
     return dB
-
-def show_power(ax, z, dBrange = 10, mask = None):
-    p = np.abs(z)**2
-    pvt = np.mean(p + 0.001, axis = 1)
-    p = p / pvt[:,None]
-    alpha_mask = np.zeros_like(p) + 1
-    if(mask is not None):
-        p_masked = np.ma.masked_array(p, mask)
-        dB = calc_dB(p_masked, dBrange = dBrange, rel_to_max = True)
-        im = ax.imshow(dB, origin="lower", aspect="auto", 
-                        cmap="grey", interpolation="none", alpha = 0.6)
-        p = np.ma.masked_array(p, (mask==False))
-    dB = calc_dB(p, dBrange = dBrange, rel_to_max = True)
-    im = ax.imshow(dB, origin="lower", aspect="auto", 
-                    cmap="inferno", interpolation="none", alpha = 0.8)
     
 def show_llr(ax,llr, tone):
     ax.barh(range(len(llr)), llr, align='edge')
     ax.set_ylim(0,len(llr))
     ax.set_xlim(-10,10)
 
-def get_llr(zf):
-    hops = np.array([i*hps + hps//2 for i in range(int(zf.shape[0]//hps))])
-    freqs = np.array([i*bpt + bpt//2 for i in range(int(zf.shape[1]//bpt))])
-    p = np.abs(zf[hops,:][:,freqs])**2
+def get_llr(p):
+    hops = np.array([i*hps + hps//2 for i in range(int(p.shape[0]//hps))])
+    freqs = np.array([i*bpt + bpt//2 for i in range(int(p.shape[1]//bpt))])
+    p = p[hops,:][:,freqs]
     pvt = np.mean(p + 0.001, axis = 1)
     p = p / pvt[:,None]
     p = calc_dB(p, dBrange = 30)
@@ -121,36 +103,35 @@ def get_llr(zf):
     llr = 3.8*llr/np.std(llr)
     return llr, np.argmax(p, axis = 1)
 
-def show_spectrum(zf, dBrange = 40):
-    fig,ax = plt.subplots( figsize = (10,5))
-    show_power(ax, zf, dBrange = dBrange)
-    plt.show()
-
-def show_sig(zf, signal, dBrange = 10, symbols = [0,79]):
-    hop_idxs = np.array(range(signal[1]-hps//2, signal[1]+79*hps-hps//2))
-    hop_idxs = np.clip(hop_idxs, 0, zf.shape[0]-1)
-    zf = zf[hop_idxs, signal[0]:signal[0]+8*bpt]
-    mask = create_mask(signal[2])
+def show_sig(p1, dBrange, h0_idx, symbols):
     fig,axs = plt.subplots(1,2, figsize = (5,10))
-    z = zf[symbols[0]*hps :symbols[1]*hps, :]
-    mask = mask[symbols[0]*hps :symbols[1]*hps, :]
-    show_power(axs[0], z, dBrange = dBrange, mask = mask)
-    llr, tone = get_llr(z)
-    show_llr(axs[1],llr, tone)
-    fig.suptitle(signal[2])
+    pvt = np.mean(p1 + 0.001, axis = 1)
+    p = p1 / pvt[:,None]
+    dB = calc_dB(p, dBrange = dBrange, rel_to_max = True)
+    im = axs[0].imshow(dB, origin="lower", aspect="auto", 
+                    cmap="inferno", interpolation="none", alpha = 0.8)
+    ax2 = plt.gca()
+    for i, t in enumerate(symbols):
+        edge = 'g' if (i<=7) or i>=72 or (i>=36 and i<=43) else 'r'
+        rect = patches.Rectangle((bpt*t -bpt//2, hps*i + h0_idx - hps//2),bpt,hps,linewidth=2,edgecolor=edge,facecolor='none')
+        axs[0].add_patch(rect)
+   # fig.suptitle(f"{signal[1]}\n{signal[0]*6.25/bpt:5.1f}Hz {0.16*signal[1]/hps:5.2f}s")
     plt.show()
 
 
-signal_info_list = [(1233, 33, 'W1FC F5BZB -08'), (1034, 23, 'WM3PEN EA6VQ -09'), (574, 1, 'CQ F5RXL IN94'), (306, 33, 'N1JFU EA6EE R-07'), (346, 29, 'A92EE F5PSR -14'), (1293, 21, 'K1BZM EA3GP -09'), (191, 33, 'W0RSJ EA3BMU RR73'), (282, 34, 'K1JT HA0DU KN07'), (1311, 37, 'W1DIG SV9CVY -14'), (790, 28, 'K1JT EA3AGB -15'), (1368, 31, 'XE2X HA2NP RR73'), (1209, 56, 'K1BZM EA3CJ JN01'), (1220, 49, 'WA2FZW DL5AXX RR73'), (1073, 33, 'N1API HA6FQ -23'), (222, 33, 'N1PJT HB9CQK -10'), (725, 46, 'N1API F2VX 73'), (977, 27, 'K1JT HA5WA 73'), (225, 39, 'KD2UGC F6GCP R-23'), (1094, 28, 'CQ EA2BFM IN83')]
-
+signal_info_list = [(2571, 'W1FC F5BZB -08'), (2157, 'WM3PEN EA6VQ -09')]
+                    
 audio_samples = read_wav("../data/210703_133430.wav")
-zf, pf = get_spectrum(audio_samples)
-#show_spectrum(zf)
+pf = get_spectrum(audio_samples)
 
 for signal in signal_info_list:
-    tsyncs = get_tsyncs(pf, signal[0])
-    signal_info = (int((bpt*signal[0])/3), tsyncs[1][0],signal[2])
-    show_sig(zf, signal_info, dBrange = 20, symbols = [0,79])
+    freq, msg = signal
+    f0_idx = int(bpt*freq/6.25)
+    p = pf[:, f0_idx:f0_idx+8*bpt]
+    tsyncs = get_tsyncs(p)
+    h0_idx = tsyncs[0][0]
+    symbols = create_symbols(msg)
+    show_sig(p, 20, h0_idx, symbols)
 
 gray_seq = [0,1,3,2,5,6,4,7]
 gray_map = np.array([[0,0,0],[0,0,1],[0,1,1],[0,1,0],[1,1,0],[1,0,0],[1,0,1],[1,1,1]])
