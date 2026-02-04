ac78da9898bfdb66145044545396fe5bab64d74b
G1OJS <140509227+G1OJS@users.noreply.github.com>
2026-02-04 10:26:06 +0000

    results benchmark

--- PyFT8/candidate.py
+++ PyFT8/candidate.py
@@ -7,10 +7,9 @@ from PyFT8.ldpc import LdpcDecoder
 from PyFT8.osd import osd_decode_minimal
 
 params = {
-'MIN_LLR0_SD': 1,                # global minimum llr_sd
-'MIN_SNR': -24,
-'LDPC_CONTROL': (33, 10, 4),         # max ncheck0, 
-'OSD_CONTROL': (25, [30,20]) # min llr_sd, max llr_sd, L(order)
+'MIN_LLR0_SD': 0.8,            # global minimum llr_sd
+'LDPC_CONTROL': (35, 10),      # max ncheck0, max iterations
+'OSD_CONTROL': (10, [30,20,5]) # max ncheck, L(order)
 }
 
 class Candidate:
@@ -30,43 +29,25 @@ class Candidate:
         self.snr = -30
         self.ldpc = LdpcDecoder()
 
-    def _flip_bits(self, llr, ncheck, width, nbits, keep_best = False):
-        import itertools
-        cands = np.argsort(np.abs(llr))
-        idxs = cands[:nbits]
-        
-        best = {'llr':llr.copy(), 'nc':ncheck}
-        for k in range(1, width + 1):
-            for comb in itertools.combinations(range(len(idxs)), k):
-                llr[idxs[list(comb)]] *= -1
-                n = self.ldpc.calc_ncheck(llr)
-                if n < best['nc']:
-                    best = {'llr':llr.copy(), 'nc':n}
-                    if n == 0:
-                        return best['llr'], 0
-                if n >= best['nc'] or not keep_best:
-                    llr[idxs[list(comb)]] *= -1
-        return best['llr'], best['nc']
-
     def _get_llr(self, pgrid_main, h0_idx, hps, freq_idxs, payload_symb_idxs, target_params = (3.3, 3.7)):
         hops = np.array([h0_idx + hps* s for s in payload_symb_idxs])
         praw = pgrid_main[np.ix_(hops, freq_idxs)]
-        pgrid = np.clip(praw, np.max(praw)-120, None)
-        snr = int(np.max(pgrid)) - 107
-        if(snr >= params['MIN_SNR']):
-            llra = np.max(pgrid[:, [4,5,6,7]], axis=1) - np.max(pgrid[:, [0,1,2,3]], axis=1)
-            llrb = np.max(pgrid[:, [2,3,4,7]], axis=1) - np.max(pgrid[:, [0,1,5,6]], axis=1)
-            llrc = np.max(pgrid[:, [1,2,6,7]], axis=1) - np.max(pgrid[:, [0,3,4,5]], axis=1)
-            llr0 = np.column_stack((llra, llrb, llrc))
-            llr0 = llr0.ravel() / 10
-            llr0_sd = int(0.5+100*np.std(llr0))/100.0
-            if (llr0_sd > params['MIN_LLR0_SD']):
-                llr0 = target_params[0] * llr0 / llr0_sd
-                llr0 = np.clip(llr0, -target_params[1], target_params[1])
-                snr = int(np.clip(snr, -24, 24))
+        p_max = np.max(praw)
+        snr = int(p_max) - 107
+        pgrid = praw - p_max
+        pgrid = np.clip(pgrid, -80, 0)
+        llra = np.max(pgrid[:, [4,5,6,7]], axis=1) - np.max(pgrid[:, [0,1,2,3]], axis=1)
+        llrb = np.max(pgrid[:, [2,3,4,7]], axis=1) - np.max(pgrid[:, [0,1,5,6]], axis=1)
+        llrc = np.max(pgrid[:, [1,2,6,7]], axis=1) - np.max(pgrid[:, [0,3,4,5]], axis=1)
+        llr0 = np.column_stack((llra, llrb, llrc))
+        llr0 = llr0.ravel() / 10
+        llr0_sd = int(0.5+100*np.std(llr0))/100.0
+        if (llr0_sd > params['MIN_LLR0_SD']):
+            llr0 = target_params[0] * llr0 / llr0_sd
+            llr0 = np.clip(llr0, -target_params[1], target_params[1])
+            snr = int(np.clip(snr, -24, 24))
             return (llr0, llr0_sd, pgrid, snr)
-        else:
-            return ([],0,[],-30)
+        return ([],llr0_sd,[],-30)
         
     def demap(self, spectrum):
         self.demap_started = time.time()
@@ -76,7 +57,7 @@ class Candidate:
         demap1 = self._get_llr(spectrum.audio_in.pgrid_main, h1, spectrum.hops_persymb, self.freq_idxs, spectrum.sigspec.payload_symb_idxs)
         sync_idx =  0 if demap0[1] > demap1[1] else 1
         demap = [demap0, demap1][sync_idx]
-        quality_too_low = (demap[1] == 0 )
+        quality_too_low = (demap[1] <= params['MIN_LLR0_SD'])
         if(quality_too_low):
             self._record_state("I", final = True)
             self.demap_completed = time.time()
