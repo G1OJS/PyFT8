57d155bd419e6babdc0a7e150137015cd10c8bfe
G1OJS <140509227+G1OJS@users.noreply.github.com>
2026-02-06 09:03:29 +0000

    tidy demap & decode code

--- PyFT8/candidate.py
+++ PyFT8/candidate.py
@@ -8,39 +8,31 @@ from PyFT8.osd import osd_decode_minimal
 
 params = {
 'MIN_SNR': -20,                # global min snr
-'MIN_LLR0_SD': 0.6,            # global minimum llr_sd
-'LDPC_CONTROL': (36, 12),      # max ncheck0, max iterations
-'OSD_CONTROL': (20, [30,20,3]) # max ncheck, L(order)
+'MIN_LLR0_SD': 0.65,           # global minimum llr_sd
+'LDPC_CONTROL': (45, 12),      # max ncheck0, max iterations
+'OSD_CONTROL': (0.8, [40,20])  # max llr0_sd, L(order)
 }
 
 class Candidate:
     def __init__(self, testing = False):
 
-        self.dedupe_key = ""
         self.demap_started, self.demap_completed, self.decode_completed = None, None, None
-        self.cyclestart_str = ""
-        self.dt = 0
-        self.td = 0
-        self.fHz = 0
-        self.ncheck, self.ncheck0 = 99, 99
-        self.llr0_sd = 0
-        self.llr = []
-        self.llr0 = []
-        self.decode_path = ""
+        self.dt, self.td, self.fHz = 0, 0, 0
+        self.decode_path = ''
+        self.cyclestart_str = ''
         self.msg = ''
-        self.snr = -30
         self.ldpc = LdpcDecoder()
 
-    def _get_llr(self, pgrid_main, h0_idx, hps, freq_idxs, payload_symb_idxs, target_params = (3.3, 3.7)):
-        hops = np.array([h0_idx + hps* s for s in payload_symb_idxs])
-        praw = pgrid_main[np.ix_(hops, freq_idxs)]
-        p_max = np.max(praw)
-        snr = int( p_max - np.min(praw) - 58)
-        pgrid = praw - p_max
-        pgrid = np.clip(pgrid, -80, 0)
-        llra = np.max(pgrid[:, [4,5,6,7]], axis=1) - np.max(pgrid[:, [0,1,2,3]], axis=1)
-        llrb = np.max(pgrid[:, [2,3,4,7]], axis=1) - np.max(pgrid[:, [0,1,5,6]], axis=1)
-        llrc = np.max(pgrid[:, [1,2,6,7]], axis=1) - np.max(pgrid[:, [0,3,4,5]], axis=1)
+    def _get_llr(self, spectrum, h0_idx, target_params = (3.3, 3.7)):
+        hops = np.array([h0_idx + spectrum.hops_persymb * s for s in spectrum.sigspec.payload_symb_idxs])
+        p_dB = spectrum.audio_in.pgrid_main[np.ix_(hops, self.freq_idxs)]
+        p_max = np.max(p_dB)
+        snr = int( p_max - np.min(p_dB) - 58)
+        p = p_dB - p_max
+        p = np.clip(p, -80, 0)
+        llra = np.max(p[:, [4,5,6,7]], axis=1) - np.max(p[:, [0,1,2,3]], axis=1)
+        llrb = np.max(p[:, [2,3,4,7]], axis=1) - np.max(p[:, [0,1,5,6]], axis=1)
+        llrc = np.max(p[:, [1,2,6,7]], axis=1) - np.max(p[:, [0,3,4,5]], axis=1)
         llr0 = np.column_stack((llra, llrb, llrc))
         llr0 = llr0.ravel() / 10
         llr0_sd = int(0.5+100*np.std(llr0))/100.0
@@ -48,31 +40,16 @@ class Candidate:
             llr0 = target_params[0] * llr0 / llr0_sd
             llr0 = np.clip(llr0, -target_params[1], target_params[1])
             snr = int(np.clip(snr, -24, 24))
-            return (llr0, llr0_sd, pgrid, snr)
-        return ([],llr0_sd,[],-30)
+            return llr0, llr0_sd, p_dB, snr
+        return [], 0, p_dB, -30
         
     def demap(self, spectrum):
         self.demap_started = time.time()
         h0, h1 = self.syncs[0]['h0_idx'], self.syncs[1]['h0_idx']
         if(h0 == h1): h1 = h0 +1
-        demap0 = self._get_llr(spectrum.audio_in.pgrid_main, h0, spectrum.hops_persymb, self.freq_idxs, spectrum.sigspec.payload_symb_idxs)
-        demap1 = self._get_llr(spectrum.audio_in.pgrid_main, h1, spectrum.hops_persymb, self.freq_idxs, spectrum.sigspec.payload_symb_idxs)
-        sync_idx =  0 if demap0[1] > demap1[1] else 1
-        demap = [demap0, demap1][sync_idx]            
-        quality_too_low = (len(demap[0])==0)
-        if(quality_too_low):
-            self._record_state("I", final = True)
-            self.demap_completed = time.time()
-            return
-        self.h0_idx = self.syncs[sync_idx]['h0_idx']
-        self.sync_score = self.syncs[sync_idx]['score']
-        self.dt = int(0.5+100*self.syncs[sync_idx]['dt'])/100.0
-        self.p_dB = 10*demap[2]
-        self.llr0, self.llr0_sd, self.pgrid, self.snr = demap
-        self.ncheck0 = self.ldpc.calc_ncheck(self.llr0)
-        self.llr = self.llr0.copy()
-        self.ncheck = self.ncheck0
-        self._record_state("I")
+        demap0, demap1 = self._get_llr(spectrum, h0), self._get_llr(spectrum, h1)
+        self.sync_idx =  0 if demap0[1] > demap1[1] else 1
+        self.llr0, self.llr0_sd, self.p_dB, self.snr = [demap0, demap1][self.sync_idx]
         self.demap_completed = time.time()
 
     def _record_state(self, actor_code, final = False):
@@ -82,31 +59,38 @@ class Candidate:
             self.decode_completed = time.time()
 
     def decode(self):
-        if not self.decode_completed and params['LDPC_CONTROL'][0] > self.ncheck > 0:
-            for it in range(params['LDPC_CONTROL'][1]):
-                self.llr, self.ncheck = self.ldpc.do_ldpc_iteration(self.llr)
-                self._record_state("L")
-                if(self.ncheck == 0):
-                    break
+        
+        quality_too_low = (len(self.llr0)==0)
+        if(quality_too_low):
+            self.ncheck0, self.ncheck = 99, 99
+            self._record_state("I", final = True)
+            return
+        self.ncheck0 = self.ldpc.calc_ncheck(self.llr0)
+        self.ncheck = self.ncheck0
+        codeword_bits = []
+        self._record_state("I")
 
-        if(self.ncheck == 0):  
-            codeword_bits = (self.llr > 0).astype(int).tolist()
-            self.check_crc_and_get_message(codeword_bits)
+        if self.ncheck > 0:
+            self.llr = self.llr0.copy()
+            if self.ncheck <= params['LDPC_CONTROL'][0]:
+                for it in range(params['LDPC_CONTROL'][1]):
+                    self.llr, self.ncheck = self.ldpc.do_ldpc_iteration(self.llr)
+                    self._record_state("L")
+                    if(self.ncheck == 0):
+                        codeword_bits = (self.llr > 0).astype(int).tolist()
+                        break
 
-        if(0 < self.ncheck < params['OSD_CONTROL'][0]):
-            if(len(self.llr) == 174 and len(self.llr0)==174):
+        if self.ncheck > 0:
+            if(self.llr0_sd < params['OSD_CONTROL'][0]):
                 reliab_order = np.argsort(np.abs(self.llr))[::-1]
                 codeword_bits = osd_decode_minimal(self.llr0, reliab_order, Ls = params['OSD_CONTROL'][1])
-                self.check_crc_and_get_message(codeword_bits)
-                if(self.msg):
-                    self.ncheck = 0
                 self._record_state("O")
 
-        self._record_state("M" if self.msg else "_", final = True)
-
-    def check_crc_and_get_message(self, codeword_bits):
-        if check_crc_codeword_list(codeword_bits):
-            if(any(codeword_bits[:77])):
+        if(any(codeword_bits[:77])):
+            if check_crc_codeword_list(codeword_bits):
                 self.msg = FT8_unpack(codeword_bits[:77])
+                self.h0_idx = self.syncs[self.sync_idx]['h0_idx']
+                self.sync_score = self.syncs[self.sync_idx]['score']
+                self.dt = int(0.5+100*self.syncs[self.sync_idx]['dt'])/100.0  
 
-
+        self._record_state("M" if self.msg else "_", final = True)
