7b299f0ac8254b1a508126edd84260dbee7e9c47
G1OJS <140509227+G1OJS@users.noreply.github.com>
2026-02-11 21:37:26 +0000

    tidy spectrum

--- PyFT8/spectrum.py
+++ PyFT8/spectrum.py
@@ -9,7 +9,6 @@ class Spectrum:
         self.sigspec = sigspec
         self.sample_rate = sample_rate
         self.fbins_pertone = fbins_pertone
-        self.max_freq = max_freq
         self.hops_persymb = hops_persymb
         self.hops_percycle = int(self.sigspec.cycle_seconds * self.sigspec.symbols_persec * self.hops_persymb)
         self.audio_in = AudioIn(self.sigspec.cycle_seconds, self.hops_percycle, self.sigspec.symbols_persec, hops_persymb, fbins_pertone, max_freq)
@@ -18,11 +17,9 @@ class Spectrum:
         self.df = max_freq / (self.nFreqs -1)
         self.fbins_per_signal = self.sigspec.tones_persymb * self.fbins_pertone
         self.hop_idxs_Costas =  np.arange(self.sigspec.costas_len) * self.hops_persymb
-        self.nhops_costas = self.sigspec.costas_len * self.hops_persymb
         self.h_search1 = int(4.6/self.dt)
         self.h_search2 = int(11/self.dt)
         self.search_hops_range = range(int((-1.7 + 0.7) / self.dt), int((3.2 + 0.7) / self.dt))
-        self.occupancy = np.zeros(self.nFreqs)
         self.csync_flat = self.make_csync(sigspec)
         payload_symb_idxs = list(range(7, 36)) + list(range(43, 72))
         data_symb_idxs = list(range(7, 36)) + list(range(43, 45))
@@ -37,10 +34,10 @@ class Spectrum:
             csync[sym_idx, sigspec.costas_len*self.fbins_pertone:] = 0
         return csync.ravel()
 
-    def get_sync(self, f0_idx, pnorm, sync_idx):
+    def get_sync(self, f0_idx, dB, sync_idx):
         best_sync = {'h0_idx':0, 'score':0, 'dt': 0}
         for h0_idx in self.search_hops_range:
-            sync_score = float(np.dot(pnorm[h0_idx + self.hop_idxs_Costas + sync_idx * 36 * self.hops_persymb ,  :].ravel(), self.csync_flat))
+            sync_score = float(np.dot(dB[h0_idx + self.hop_idxs_Costas + sync_idx * 36 * self.hops_persymb ,  :].ravel(), self.csync_flat))
             test_sync = {'h0_idx':h0_idx, 'score':sync_score, 'dt': h0_idx * self.dt - 0.7}
             if test_sync['score'] > best_sync['score']:
                 best_sync = test_sync
