978cbfe13f09e0ac44c97a0ffc60d8ac3dfd9983
G1OJS <140509227+G1OJS@users.noreply.github.com>
2026-03-07 00:08:56 +0000

    Update pyft8.py

--- PyFT8/pyft8.py
+++ PyFT8/pyft8.py
@@ -86,6 +86,8 @@ def on_msg_click(clicked_msg, msg_origin, msg_params):
         return
 
     if call_a == my_station['c']:
+        if qso.times['time_on'] is None:
+            qso.times['time_on'] = time.gmtime()
         if qso.oStation['c'] is None:
             qso.oStation['c'] = call_b
         if isGrid(grid_rpt):
