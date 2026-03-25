d4d2a056968fd786fd7e1094b53a32543a041ed2
G1OJS <140509227+G1OJS@users.noreply.github.com>
2026-03-25 22:02:42 +0000

    Simplify database checks

--- PyFT8/mqtt.py
+++ PyFT8/mqtt.py
@@ -12,12 +12,15 @@ class PSKR_MQTT_listener:
         mqttc.on_message = self.on_message
         mqttc.connect("mqtt.pskreporter.info", 1883, 60)
         self.cache = {}
+        self.home_call_info = {}
+        self.home_activity = {}
         threading.Thread(target = mqttc.loop_forever, daemon = True).start()
         
     def on_connect(self, client, userdata, flags, reason_code, properties):
         #pskr/filter/v2/{band}/{mode}/{sendercall}/{receivercall}/{senderlocator}/{receiverlocator}/{sendercountry}/{receivercountry}
         print(f"[MQTT] Connected with result code {reason_code}")
         print(f"[MQTT] Requesting mqtt feed for {self.home_square}")
+        client.subscribe(f"pskr/filter/v2/+/FT8/+/+/{self.home_square}/#")
         client.subscribe(f"pskr/filter/v2/+/FT8/+/+/+/{self.home_square}/#")
 
     def on_message(self, client, userdata, msg):
@@ -26,17 +29,29 @@ class PSKR_MQTT_listener:
         except:
             return
         sc, rc = (d['sc'], d['sl']), (d['rc'], d['rl'])
-        for c in [sc, rc]:
-            #print(f"[MQTT] {len(self.cache)} {sc} - > {rc} on {d['b']} {d['md']}")
-            if c[0] not in self.cache:
-                self.cache[c[0]]=c[1]
-
+        for i, c in enumerate([sc, rc]):
+            call, loc = c
+            if call not in self.cache:
+                self.cache[call] = loc
+            if self.home_square in loc:
+                key = f"{d['b']}_{['Tx','Rx'][i]}_{call}"
+                if not key in self.home_call_info:
+                    self.home_call_info[key] = 0
+                self.home_call_info[key] +=1
+                
 if __name__ == '__main__':
     pskr = PSKR_MQTT_listener("IO90")
 
     while True:
         time.sleep(5)
-        print(pskr.cache)
+        for c_info in pskr.home_call_info:
+            c = c_info.split("_")[0]
+            pskr.home_activity[c] = [0,0]
+        for c_info in pskr.home_call_info:
+            c, txrx = c_info.split("_")[:2]
+            pskr.home_activity[c][['Tx','Rx'].index(txrx)] +=1
+        print(pskr.home_activity)
+
     
 
 
