639863f497c2d7afd18eb3d41c641be7b85d54e3
G1OJS <140509227+G1OJS@users.noreply.github.com>
2026-03-26 16:43:01 +0000

    Update pyft8.py

--- PyFT8/pyft8.py
+++ PyFT8/pyft8.py
@@ -51,7 +51,6 @@ class ADIF:
         self.cache = self._build_cache()
               
     def log(self, times, band_info, mStation, oStation, rpts):
-        import datetime
         log_dict = {'call':oStation['c'], 'gridsquare':oStation['g'], 'mode':'FT8',
         'operator':mStation['c'], 'station_callsign':mStation['c'], 'my_gridsquare':mStation['g'], 
         'rst_sent':rpts['sent'], 'rst_rcvd':rpts['rcvd'], 
@@ -64,7 +63,7 @@ class ADIF:
                 f.write(f"<{k}:{len(v)}>{v} ")
             f.write(f"<eor>\n")
         cbm = log_dict['call'] + "_" + log_dict['band'] + "_FT8"
-        tm = time.mktime(datetime.datetime.strptime(d+t, "%Y%m%d%H%M%S").timetuple())
+        tm = time.time()
         cache[log_dict['call']] = tm
         cache[cbm] = tm
         console_print(f"Logged QSO with {oStation['c']}")
