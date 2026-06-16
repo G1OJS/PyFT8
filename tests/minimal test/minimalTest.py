5c7fe26aea7bff5757a08083e178916ff50b82c1
G1OJS <140509227+G1OJS@users.noreply.github.com>
2026-06-16 17:57:06 +0100

    add minimal test

---
+++ tests/minimal test/minimalTest.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import os
+import sys
+import threading
+
+from PyFT8.receiver import Receiver, AudioIn
+nDecodes = 0
+
+def on_decode(c):
+    global nDecodes
+    msg = getattr(c, 'msg', None)
+    if msg is not None:
+        nDecodes += 1
+        print(f"{nDecodes:03d}:{msg} {c.h0_idx, c.f0_idx}")
+   
+def donothing(*args):
+    return
+
+wav_file = os.path.join('./', 'test_01.wav')
+audio_in = AudioIn(3100, [wav_file])
+rx = Receiver(audio_in, [200, 3100], on_decode, None)
+audio_in.start_wav_load()
+
+
