db430bbdb23529043674cfe31cb8f83bd7ae2240
G1OJS <140509227+G1OJS@users.noreply.github.com>
2026-06-26 11:17:25 +0100

    Update PyFT8-like-with-phase-autocorrelate.py

--- tests/standalone/PyFT8-like-with-phase-autocorrelate.py
+++ tests/standalone/PyFT8-like-with-phase-autocorrelate.py
@@ -8,7 +8,7 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 repo_root = os.path.dirname(script_dir)
 sys.path.insert(0, repo_root)
 
-HPS=6
+HPS=4
 BPT=2
 SYM_RATE =6.25
 SAMP_RATE=12000
@@ -24,7 +24,6 @@ BASE_PAYLOAD_HOPS = np.array([HPS * s for s in symbol_idxs])
 LAST_BASE_PAYLOAD_HOP = BASE_PAYLOAD_HOPS[-1]
 COSTAS = [3,1,4,0,6,5,2]
 BASE_COSTAS_HOPS =  np.arange(7) * HPS
-HOPS_PER_CYCLE = int(T_CYC * SYM_RATE * HPS)
 
 #=========== Unpacking functions ========================================
 def get_bitfields(bits, lengths):
@@ -155,45 +154,38 @@ class LdpcDecoder:
         llr += update_collector
         return llr, self.calc_ncheck(llr)
 
-#============== AUDIO ========================================================
-class AudioIn:
-    def __init__(self, wav_path, freq_range=[00, 3100]):
-        self.fft_len = int( BPT * SAMP_RATE // SYM_RATE)
-        fft_out_len = self.fft_len // 2 + 1
-        max_freq = freq_range[1]
-        self.nFreqs = int(fft_out_len * 2 * max_freq / SAMP_RATE)
-        self.audio_buffer = np.zeros(self.fft_len, dtype=np.float32)
-        self.fft_in = np.zeros(self.fft_len, dtype=np.float32)
-        self.fft_window = fft_window=np.hanning(self.fft_len).astype(np.float32)
-        self.zgrid_main = np.ones((HOPS_PER_CYCLE * 2, self.nFreqs), dtype = np.complex64) 
-        samples_perhop = int(SAMP_RATE / (SYM_RATE * HPS))
-        self.zgrid_main_ptr = 0
-
-        wf = wave.open(wav_path, "rb")
-        frames = wf.readframes(samples_perhop)
-        while frames:
-            self.process_hop(frames)
-            frames = wf.readframes(samples_perhop)
-        wf.close()
-                                   
-    def process_hop(self, in_data):
-        samples = np.frombuffer(in_data, dtype=np.int16).astype(np.float32)
-        ns = len(samples)
-        self.audio_buffer[:-ns] = self.audio_buffer[ns:]
-        self.audio_buffer[-ns:] = samples
-        np.multiply(self.audio_buffer, self.fft_window, out=self.fft_in)
-        self.zgrid_main[self.zgrid_main_ptr, :] = np.fft.rfft(self.fft_in)[:self.nFreqs]
-        self.zgrid_main_ptr +=1
-
-def z2dB(z):
-    return 10*np.log10(z.real*z.real + z.imag*z.imag + 1e-12)
+fft_len = int( BPT * SAMP_RATE // SYM_RATE)
+fft_out_len = fft_len // 2 + 1
+max_freq = 3500
+nFreqs = int(fft_out_len * 2 * max_freq / SAMP_RATE)
+audio_buffer = np.zeros(fft_len, dtype=np.float32)
+fft_in = np.zeros(fft_len, dtype=np.float32)
+fft_window = fft_window=np.hanning(fft_len).astype(np.float32)
+hops_per_grid = T_CYC * SYM_RATE * HPS
+if (int(hops_per_grid) != hops_per_grid):
+    print("Warning - non-integer number of hops per grid")
+hops_per_grid = int(hops_per_grid)
+zgrid_main = np.ones((hops_per_grid, nFreqs), dtype = np.complex64) 
+samples_perhop = int(SAMP_RATE / (SYM_RATE * HPS))
+zgrid_main_ptr = 0
 
 def get_messages(wav_file):
-    audio_in = AudioIn(wav_file)
+    global audio_buffer, fft_in, zgrid_main, zgrid_main_ptr
 
-    nFreqs = audio_in.nFreqs
+    wf = wave.open(wav_file, "rb")
+    frames = wf.readframes(samples_perhop)
+    while frames:
+        samples = np.frombuffer(frames, dtype=np.int16).astype(np.float32)
+        ns = len(samples)
+        audio_buffer[:-ns] = audio_buffer[ns:]
+        audio_buffer[-ns:] = samples
+        np.multiply(audio_buffer, fft_window, out=fft_in)
+        zgrid_main[zgrid_main_ptr, :] = np.fft.rfft(fft_in)[:nFreqs]
+        zgrid_main_ptr = (zgrid_main_ptr+1) % hops_per_grid
+        frames = wf.readframes(samples_perhop)
+    wf.close()
+
     dt = 1.0 / (SYM_RATE * HPS)
-
     csync = np.full((7, 7 * BPT), -1/6, np.float32)
     for sym_idx, tone in enumerate([3,1,4,0,6,5,2]):
         fbins = range(tone * BPT, (tone+1) * BPT)
@@ -204,7 +196,7 @@ def get_messages(wav_file):
     target_params = [3.5, 3.7]
 
     for fb in range(nFreqs - 8 * BPT):
-        zstrip = audio_in.zgrid_main[:, fb: fb+8*BPT]
+        zstrip = zgrid_main[:, fb: fb+8*BPT]
        # tsum = np.sum(zstrip[:,BASE_FREQ_IDXS], axis = 1)
        # zstrip = zstrip * (np.abs(tsum) / tsum)[:,None]
        # dBgrid = 20*np.log10(np.abs(zstrip.real) + 1e-12)
@@ -217,7 +209,8 @@ def get_messages(wav_file):
             test_sync = {'h0_idx':h0_idx, 'score':sync_score}
             if test_sync['score'] > sync['score']:
                 sync = test_sync
-        dBgrid = dBgrid[sync['h0_idx'] + BASE_PAYLOAD_HOPS, :]
+        hops = [h % hops_per_grid for h in sync['h0_idx'] + BASE_PAYLOAD_HOPS]
+        dBgrid = dBgrid[hops, :]
         dBgrid = dBgrid[:, BASE_FREQ_IDXS]
 
         pmax = np.max(dBgrid)
@@ -251,6 +244,7 @@ def get_messages(wav_file):
                     if(msg not in messages):
                         h0_idx = sync['h0_idx']
                         messages[msg] = f"{1+len(messages):03d}:{msg:25s}{6.25*fb/BPT:7.1f}, {0.16*h0_idx/HPS - 0.5:5.1f} {llr_sd:5.1f} {nits:03d}"
+                        print(messages[msg])
     return messages
 
 data_folder = "C:/Users/drala/Documents/Projects/GitHub/PyFT8/tests/data/ft8_lib_20m_busy"
