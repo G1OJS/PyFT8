8b243db465be700cfcb0d7c07bfb86f1920544a1
G1OJS <140509227+G1OJS@users.noreply.github.com>
2026-06-29 22:14:19 +0100

    prep test.py for time domain freq and time tweaking

--- tests/standalone/test.py
+++ tests/standalone/test.py
@@ -10,27 +10,19 @@ sys.path.insert(0, repo_root)
 
 HPS=4
 BPT=2
-SYM_RATE =6.25
+SYM_RATE = 6.25
+N_SYMS = 79
 SAMP_RATE=12000
+SAMPS_PER_SYM = 1920
 T_CYC=15
 t2h = HPS/0.16
-MIN_LLR_SD= 0.0
+MIN_SYNC_SCORE = 20
 LDPC_CONTROL = (55, 15) 
 H0_RANGE = [int(-2 *t2h), int(5*t2h)]
 
-symbol_idxs = list(range(7, 36)) + list(range(43, 72))
-BASE_PAYLOAD_HOPS = np.array([HPS * s for s in symbol_idxs])
-LAST_BASE_PAYLOAD_HOP = BASE_PAYLOAD_HOPS[-1]
+PAYLOAD_SYMBOLS = list(range(7, 36)) + list(range(43, 72))
 COSTAS = [3,1,4,0,6,5,2]
 BASE_COSTAS_HOPS =  np.arange(7) * HPS
-
-c = BPT//2
-A1 = [a * BPT +c for a in [4,5,6,7]]
-A0 = [a * BPT +c for a in [0,1,2,3]]
-B1 = [a * BPT +c for a in [2,3,4,7]]
-B0 = [a * BPT +c for a in [0,1,5,6]]
-C1 = [a * BPT +c for a in [1,2,6,7]]
-C0 = [a * BPT +c for a in [0,3,4,5]]
 
 #=========== Unpacking functions ========================================
 def get_bitfields(bits, lengths):
@@ -161,24 +153,55 @@ class LdpcDecoder:
         llr += update_collector
         return llr, self.calc_ncheck(llr)
 
-fft_len = int( BPT * SAMP_RATE // SYM_RATE)
-fft_out_len = fft_len // 2 + 1
-max_freq = 3500
-nFreqs = int(fft_out_len * 2 * max_freq / SAMP_RATE)
-audio_buffer = np.zeros(fft_len, dtype=np.float32)
-fft_in = np.zeros(fft_len, dtype=np.float32)
-fft_window = fft_window=np.hanning(fft_len).astype(np.float32)
-hops_per_grid = T_CYC * SYM_RATE * HPS
-if (int(hops_per_grid) != hops_per_grid):
-    print("Warning - non-integer number of hops per grid")
-hops_per_grid = int(hops_per_grid)
-zgrid_main = np.ones((hops_per_grid, nFreqs), dtype = np.complex64) 
-samples_perhop = int(SAMP_RATE / (SYM_RATE * HPS))
-zgrid_main_ptr = 0
+
+
+def get_candidate_tfgrid(all_audio_frames, origin):
+    # get full audio spectrum (later - move outside and do once on wav load)
+    fft_len = 192000
+    samples = np.zeros(fft_len)
+    samps_in = np.frombuffer(all_audio_frames, dtype=np.int16).astype(np.float32)
+    samples[:len(samps_in)] = samps_in 
+    all_audio_spectrum = np.fft.fft(samples)
+
+    # downsample to 32 samples per symbol
+    df = SAMP_RATE/fft_len
+    fb_0 = int(0.5 + origin['f0'] / df )
+    fb_top = int(0.5 + (origin['f0'] + 8.5*SAMP_RATE/fft_len) / df )
+    fb_bot = int(0.5 + (origin['f0'] - 1.5*SAMP_RATE/fft_len) / df )
+    fft_len = 3200
+    candidate_spectrum = np.zeros(fft_len, dtype = np.complex64)
+    candidate_spectrum[:(fb_top - fb_bot)] = all_audio_spectrum[fb_bot:fb_top]
+    np.roll(candidate_spectrum, fb_0 - fb_bot)
+    candidate_zsig = np.fft.fft(candidate_spectrum)
+
+    # get candidate symbol spectra x79 with df = 1 tone spacing
+    candidate_tf_zgrid = np.zeros((N_SYMS, 8), dtype = np.complex64)
+    for s in range(N_SYMS):
+        i0 = int(origin['t0'] * 32 * SYM_RATE + s * 32)
+        zsymb = candidate_zsig[i0:i0+32]
+        if(zsymb.shape[0] == 32):
+            candidate_tf_zgrid[s, :] = np.fft.fft(zsymb)[:8]
+
+    return candidate_tf_zgrid        
 
 def get_messages(wav_file):
-    global audio_buffer, fft_in, zgrid_main, zgrid_main_ptr
 
+# Open wav file and process to tf grid with dt = 20ms (4 per symbol) and df = 3.125Hz (2 per tone)
+    fft_len = int( BPT * SAMP_RATE // SYM_RATE)
+    fft_out_len = fft_len // 2 + 1
+    max_freq = 3500
+    nFreqs = int(fft_out_len * 2 * max_freq / SAMP_RATE)
+    audio_buffer = np.zeros(fft_len, dtype=np.float32)
+    fft_in = np.zeros(fft_len, dtype=np.float32)
+    fft_window = fft_window=np.hanning(fft_len).astype(np.float32)
+    hops_per_grid = T_CYC * SYM_RATE * HPS
+    if (int(hops_per_grid) != hops_per_grid):
+        print("Warning - non-integer number of hops per grid")
+    hops_per_grid = int(hops_per_grid)
+    zgrid_main = np.ones((hops_per_grid, nFreqs), dtype = np.complex64) 
+    samples_perhop = int(SAMP_RATE / (SYM_RATE * HPS))
+    zgrid_main_ptr = 0
+    
     wf = wave.open(wav_file, "rb")
     frames = wf.readframes(samples_perhop)
     while frames:
@@ -192,6 +215,7 @@ def get_messages(wav_file):
         frames = wf.readframes(samples_perhop)
     wf.close()
 
+# Create sync template
     dt = 1.0 / (SYM_RATE * HPS)
     csync = np.full((7, 7 * BPT), -1/6, np.float32)
     for sym_idx, tone in enumerate([3,1,4,0,6,5,2]):
@@ -199,54 +223,62 @@ def get_messages(wav_file):
         csync[sym_idx, fbins] = 1.0
     csync_flat =  csync.ravel()
 
-    messages = {}
-    target_params = [3.5, 3.7]
-
-    for fb in range(nFreqs - 8 * BPT):
+# Find candidate origins on the coarse tf grid
+    origins = []
+    f_min = 100
+    for fb in range(int(f_min*BPT/SYM_RATE), nFreqs - 8 * BPT):
         zstrip = zgrid_main[:, fb: fb+8*BPT]
         dBgrid = 20*np.log10(np.abs(zstrip) + 1e-12)
 
-        sync = {'h0_idx':0, 'score':0}
+        origin = {'score':0}
         for h0_idx in range(H0_RANGE[0], H0_RANGE[1]):
             sync_score = float(np.dot(dBgrid[h0_idx + BASE_COSTAS_HOPS + 36 * HPS, :7*BPT].ravel(), csync_flat))
-            test_sync = {'h0_idx':h0_idx, 'score':sync_score}
-            if test_sync['score'] > sync['score']:
-                sync = test_sync
-        hops = [h % hops_per_grid for h in sync['h0_idx'] + BASE_PAYLOAD_HOPS]
-        dBgrid = dBgrid[hops, :]
+            test_sync = {'h0_idx':h0_idx, 't0':h0_idx/(HPS * SYM_RATE), 'f0_idx':fb, 'f0':SYM_RATE * fb / BPT, 'score':sync_score}
+            if test_sync['score'] > origin['score']:
+                origin = test_sync
+        if origin['score'] > MIN_SYNC_SCORE:
+            origins.append(origin)
 
+# Open wav file again and store all samples 
+    wf = wave.open(wav_file, "rb")
+    all_audio_frames = wf.readframes(SAMP_RATE * T_CYC)
+    wf.close()
+
+    messages = {}
+    for origin in origins:
+        zcand = get_candidate_tfgrid(all_audio_frames, origin)
+        dBgrid = 20*np.log10(np.abs(zcand[PAYLOAD_SYMBOLS, :]))
         pmax = np.max(dBgrid)
         snr = np.clip(int(pmax - np.min(dBgrid) - 58), -24, 24)
         p = np.clip(dBgrid - pmax, -80, 0)
-        llra = np.max(p[:, A1], axis=1) - np.max(p[:, A0], axis=1)
-        llrb = np.max(p[:, B1], axis=1) - np.max(p[:, B0], axis=1)
-        llrc = np.max(p[:, C1], axis=1) - np.max(p[:, C0], axis=1)
+        llra = np.max(p[:, [4,5,6,7]], axis=1) - np.max(p[:, [0,1,2,3]], axis=1)
+        llrb = np.max(p[:, [2,3,4,7]], axis=1) - np.max(p[:, [0,1,5,6]], axis=1)
+        llrc = np.max(p[:, [1,2,6,7]], axis=1) - np.max(p[:, [0,3,4,5]], axis=1)
         llr = np.column_stack((llra, llrb, llrc))
         llr = llr.ravel() / 10
         llr_sd = int(0.5+100*np.std(llr))/100.0
-        llr = target_params[0] * llr / (1e-12 + llr_sd)
-        llr = np.clip(llr, -target_params[1], target_params[1])
-        if llr_sd > MIN_LLR_SD:
-            ldpc = LdpcDecoder()
-            ncheck, nits = ldpc.calc_ncheck(llr), 0
-            if 0 < ncheck <= LDPC_CONTROL[0]:
-                for ldpc_it in range(LDPC_CONTROL[1]):
-                    llr, ncheck = ldpc.do_ldpc_iteration(llr)
-                    if(ncheck == 0):
-                        nits = ldpc_it
-                        break
-            bits91_int = 0
-            for bit in (llr[:91] > 0).astype(int).tolist():
-                bits91_int = (bits91_int << 1) | bit
-            bits77_int = check_crc(bits91_int)
-            if(bits77_int):
-                msg = unpack(bits77_int)
-                if msg:
-                    msg = ' '.join(msg)
-                    if(msg not in messages):
-                        h0_idx = sync['h0_idx']
-                        messages[msg] = f"{1+len(messages):03d}:{msg:25s}{6.25*fb/BPT:7.1f}, {0.16*h0_idx/HPS - 0.5:5.1f} {llr_sd:5.1f} {nits:03d}"
-                        print(messages[msg])
+        llr = 3.5 * llr / (1e-12 + llr_sd)
+        llr = np.clip(llr, -3.7, 3.7)
+        ldpc = LdpcDecoder()
+        ncheck, nits = ldpc.calc_ncheck(llr), 0
+        if 0 < ncheck <= LDPC_CONTROL[0]:
+            for ldpc_it in range(LDPC_CONTROL[1]):
+                llr, ncheck = ldpc.do_ldpc_iteration(llr)
+                if(ncheck == 0):
+                    nits = ldpc_it
+                    break
+        bits91_int = 0
+        for bit in (llr[:91] > 0).astype(int).tolist():
+            bits91_int = (bits91_int << 1) | bit
+        bits77_int = check_crc(bits91_int)
+        if(bits77_int):
+            msg = unpack(bits77_int)
+            if msg:
+                msg = ' '.join(msg)
+                if(msg not in messages):
+                    h0_idx = origin['h0_idx']
+                    messages[msg] = f"{1+len(messages):03d}:{msg:25s}{6.25*fb/BPT:7.1f}, {0.16*h0_idx/HPS - 0.5:5.1f} {llr_sd:5.1f} {nits:03d}"
+                    print(messages[msg])
     return messages
 
 data_folder = "C:/Users/drala/Documents/Projects/GitHub/PyFT8/tests/data/ft8_lib_20m_busy"
