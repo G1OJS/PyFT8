ef1570441de161891581343e4512e824ee56d87e
G1OJS <140509227+G1OJS@users.noreply.github.com>
2026-07-01 11:16:38 +0100

    PyFT8-like-test using fine tuning and AP.py working

--- tests/standalone/PyFT8-like-test using fine tuning and AP.py
+++ tests/standalone/PyFT8-like-test using fine tuning and AP.py
@@ -7,17 +7,19 @@ import wave
 SYM_RATE =6.25
 SAMP_RATE=12000
 T_CYC=15
-zgrid_main = None
+SYM_RATE = 6.25
+SAMP_RATE=12000
+T_CYC=15
 
 from PyFT8.receiver import unpack, LdpcDecoder, check_crc
-  
+
 def get_initial_origins(wav_file ):
     global zgrid_main, HPS, BPT
     HPS=4
     BPT=2
-    H0_RANGE = [int(-2*HPS/0.16), int(5*HPS/0.16)]
     COSTAS = [3,1,4,0,6,5,2]
-    BASE_COSTAS_HOPS =  np.arange(7) * HPS
+    HOPDELAY = HPS
+    BASE_COSTAS_HOPS =  np.arange(7) * HPS + HOPDELAY
     fft_len = int( BPT * SAMP_RATE // SYM_RATE)
     fft_out_len = fft_len // 2 + 1
     max_freq = 2900
@@ -60,85 +62,126 @@ def get_initial_origins(wav_file ):
         dBgrid = 20*np.log10(np.abs(zstrip) + 1e-12)
 
         origin = {'score':0}
-        for h0_idx in range(H0_RANGE[0], H0_RANGE[1]):
-            sync_score = float(np.dot(dBgrid[h0_idx + BASE_COSTAS_HOPS + 36 * HPS, :7*BPT].ravel(), csync_flat))
+        for h0_idx in range(0, int(5*HPS/0.16)):
+            sync_score = float(np.dot(dBgrid[h0_idx + BASE_COSTAS_HOPS, :7*BPT].ravel(), csync_flat))
             test_sync = {'t0':h0_idx/(HPS * SYM_RATE), 'f0':SYM_RATE * fb / BPT, 'score':sync_score}
             if test_sync['score'] > origin['score']:
                 origin = test_sync
-        if origin['score'] > 20:
+        if origin['score'] > 2:
             origins.append(origin)
 
     return origins
 
-def get_message(wav_file, origin):
-    LDPC_CONTROL = (50, 15) 
-    PAYLOAD_SYMBOLS = list(range(7, 36)) + list(range(43, 72))
+def get_candidate_tfgrid(all_audio_spectrum, origin):
+    fft1_len = len(all_audio_spectrum)
+    N_SYMS = 79
+    
+    # downsample to 32 samples per symbol / 200 samples per sec
+    df = SAMP_RATE / fft1_len
+    fb_0 = int(0.5 + origin['f0'] / df )
+    fb_top = int(0.5 + (origin['f0'] + 8.5*SYM_RATE) / df )
+    fb_bot = int(0.5 + (origin['f0'] - 1.5*SYM_RATE) / df )
+    fft2_len = 3200
+    candidate_spectrum = np.zeros(fft2_len, dtype = np.complex64)
+    candidate_spectrum[:(fb_top - fb_bot)] = all_audio_spectrum[fb_bot:fb_top]
+    candidate_spectrum = np.roll(candidate_spectrum, -(fb_0 - fb_bot))
+    candidate_zsig = np.fft.ifft(candidate_spectrum)
 
-    c = BPT//2
-    A1 = [a * BPT +c for a in [4,5,6,7]]
-    A0 = [a * BPT +c for a in [0,1,2,3]]
-    B1 = [a * BPT +c for a in [2,3,4,7]]
-    B0 = [a * BPT +c for a in [0,1,5,6]]
-    C1 = [a * BPT +c for a in [1,2,6,7]]
-    C0 = [a * BPT +c for a in [0,3,4,5]]
-    target_params = [3.5, 3.7]
-    h0_idx = int(origin['t0'] * HPS * SYM_RATE)
-    f0_idx = int(origin['f0'] * BPT / SYM_RATE)
-    gridsize = zgrid_main.shape[0]
-    zcand = zgrid_main[[(h0_idx + HPS*s) % gridsize for s in PAYLOAD_SYMBOLS],:]
-    zcand = zcand[:, f0_idx + np.array(range(8*BPT))]
-    dBgrid = 20*np.log10(np.abs(zcand))
-    pmax = np.max(dBgrid)
-    snr = np.clip(int(pmax - np.min(dBgrid) - 58), -24, 24)
-    p = np.clip(dBgrid - pmax, -80, 0)
-    llra = np.max(p[:, A1], axis=1) - np.max(p[:, A0], axis=1)
-    llrb = np.max(p[:, B1], axis=1) - np.max(p[:, B0], axis=1)
-    llrc = np.max(p[:, C1], axis=1) - np.max(p[:, C0], axis=1)
-    llr = np.column_stack((llra, llrb, llrc))
-    llr = llr.ravel() / 10
-    llr_sd = np.std(llr)
-    llr = target_params[0] * llr / (1e-12 + llr_sd)
-    llr0 = np.clip(llr, -target_params[1], target_params[1])
-    apmag = np.max(np.abs(llr0))*1.01
+    # get candidate symbol spectra x79 with df = 1 tone spacing
+    candidate_tf_zgrid = np.ones((N_SYMS, 8), dtype = np.complex64)
+    dt = (1 / SAMP_RATE) * fft1_len / fft2_len
+    for s in range(N_SYMS):
+        i0 = int(((origin['t0'])/dt) + s * 32)
+        zsymb = candidate_zsig[i0:i0+32]
+        if(zsymb.shape[0] == 32):
+            candidate_tf_zgrid[s, :] = np.fft.fft(zsymb)[:8]
+    
+    return candidate_tf_zgrid        
 
-    ap_patterns = [
-                    [0, []],                                                            # no AP
-                    [0, [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0]],   # CQ
-                    [58,[0,1,1,1,1,1,1,0,0,1,1,1,0,1,0,1,0,0,1]],                       # RR73
-                    [58,[0,1,1,1,1,1,1,0,1,0,0,1,0,1,0,0,0,0,1]],                       # 73
-                    [58,[0,1,1,1,1,1,1,0,1,0,0,1,0,0,1,0,0,0,1]],                       # RRR
-                  ]
-    msg, ipass = None, 0
-    while (not msg) and ipass < len(ap_patterns):
-        llr = llr0
-        b0, ap_pattern = ap_patterns[ipass]
-        for b, bval in enumerate(ap_pattern):
-            llr[b0 + b] = (bval*2-1) * apmag
-        ipass += 1
-        ldpc = LdpcDecoder()
-        nits = 0
-        ncheck = ldpc.calc_ncheck(llr)
-        if 0 < ncheck <= LDPC_CONTROL[0]:
-            llr, ncheck, nits = ldpc.decode(llr)
-        bits91_int = 0
-        for bit in (llr[:91] > 0).astype(int).tolist():
-            bits91_int = (bits91_int << 1) | bit
-        bits77_int = check_crc(bits91_int)
-        if(bits77_int):
-            msg = unpack(bits77_int)
-            if msg:
-                return ' '.join(msg)
+def get_messages(wav_file):
+    # get full audio spectrum 
+    wf = wave.open(wav_file, "rb")
+    all_audio_frames = wf.readframes(SAMP_RATE * T_CYC)
+    wf.close()
+    fft1_len = 192000
+    samples = np.zeros(fft1_len)
+    samps_in = np.frombuffer(all_audio_frames, dtype=np.int16).astype(np.float32)
+    samples[:len(samps_in)] = samps_in 
+    all_audio_spectrum = np.fft.fft(samples)
 
-
-def process_file(wav_file):
     origins = get_initial_origins(wav_file)
+    LDPC_CONTROL = (55, 15) 
+    PAYLOAD_SYMBOLS = list(range(7, 36)) + list(range(43, 72))    
     messages = {}
+
+    csync = np.full((7, 7), -1/6, np.float32)
+    for sym_idx, tone in enumerate([3,1,4,0,6,5,2]):
+        csync[sym_idx, tone] = 1.0
+    csync_flat =  csync.ravel()
     for origin in origins:
-        msg = get_message(wav_file, origin)
-        if(msg and msg not in messages):
-            messages[msg] = f"{1+len(messages):03d}:{msg:25s}"
-            print(messages[msg])        
+        
+        ttweaks = np.arange(-50, 51, 10)/1000
+        scores = []
+        for ttweak in ttweaks:
+            tmp_origin = {'t0':origin['t0']+ttweak, 'f0':origin['f0'], 'score':0}
+            zcand = get_candidate_tfgrid(all_audio_spectrum, tmp_origin)
+            score = float(np.dot(np.abs(zcand[:7, :7]).ravel(), csync_flat))
+            scores.append(score)
+        ttweak = float(ttweaks[np.argmax(scores)])
+        origin = {'t0':origin['t0']+ttweak, 'f0':origin['f0'], 'score':np.max(scores)}
+        
+        ftweaks = np.arange(-2.5, 2.6, 0.5)
+        scores = []
+        for ftweak in ftweaks:
+            tmp_origin = {'t0':origin['t0'], 'f0':origin['f0']+ftweak, 'score':0}
+            zcand = get_candidate_tfgrid(all_audio_spectrum, tmp_origin)
+            score = float(np.dot(np.abs(zcand[:7, :7]).ravel(), csync_flat))
+            scores.append(score)
+        ftweak = float(ftweaks[np.argmax(scores)])
+        origin = {'t0':origin['t0'], 'f0':origin['f0']+ftweak, 'score':np.max(scores)}
+
+        zcand = get_candidate_tfgrid(all_audio_spectrum, origin)
+        p = 20*np.log10(np.abs(zcand[PAYLOAD_SYMBOLS, :]))
+        snr = np.clip(int(np.max(p) - np.min(p) - 58), -24, 24)
+        llra = np.max(p[:, [4,5,6,7]], axis=1) - np.max(p[:, [0,1,2,3]], axis=1)
+        llrb = np.max(p[:, [2,3,4,7]], axis=1) - np.max(p[:, [0,1,5,6]], axis=1)
+        llrc = np.max(p[:, [1,2,6,7]], axis=1) - np.max(p[:, [0,3,4,5]], axis=1)
+        llr = np.column_stack((llra, llrb, llrc)).ravel()
+        llr_sd = np.std(llr)
+        llr = 3.5 * llr / (1e-12 + llr_sd)        
+        llr0 = np.clip(llr, -3.7, 3.7)
+        apmag = np.max(np.abs(llr0))*1.01
+
+        ap_patterns = [
+                        [0, []],                                                            # no AP
+                        [0, [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0]],   # CQ
+                        [58,[0,1,1,1,1,1,1,0,0,1,1,1,0,1,0,1,0,0,1]],                       # RR73
+                        [58,[0,1,1,1,1,1,1,0,1,0,0,1,0,1,0,0,0,0,1]],                       # 73
+                        [58,[0,1,1,1,1,1,1,0,1,0,0,1,0,0,1,0,0,0,1]],                       # RRR
+                      ]
+        msg, ipass = None, 0
+        while (not msg) and ipass < len(ap_patterns):
+            llr = llr0
+            b0, ap_pattern = ap_patterns[ipass]
+            for b, bval in enumerate(ap_pattern):
+                llr[b0 + b] = (bval*2-1) * apmag
+            ipass += 1
+            ldpc = LdpcDecoder()
+            nits = 0
+            ncheck = ldpc.calc_ncheck(llr)
+            if 0 < ncheck <= LDPC_CONTROL[0]:
+                llr, ncheck, nits = ldpc.decode(llr)
+            bits91_int = 0
+            for bit in (llr[:91] > 0).astype(int).tolist():
+                bits91_int = (bits91_int << 1) | bit
+            bits77_int = check_crc(bits91_int)
+            if(bits77_int):
+                msg = unpack(bits77_int)
+                if not msg in messages:
+                    messages[msg] = f"{1+len(messages):03d}:{' '.join(msg):25s} {ipass:03d} {nits:03d}"
+                    print(messages[msg])
     return messages
+
 
 data_folder = "C:/Users/drala/Documents/Projects/GitHub/PyFT8/tests/data/ft8_lib_20m_busy"
 wav_folder = "C:/Users/drala/Documents/Projects/GitHub/ft8_lib/test/wav/20m_busy"
@@ -148,7 +191,7 @@ for i in range(1,39):
     test_id = f"test_{i:02d}"
     with open(f"{data_folder}/{test_id}_wsjtx_2.7.0_NORM.txt", "r") as f:
         nwsjtx = len(f.readlines())
-    messages = process_file(f"{wav_folder}/{test_id}.wav")
+    messages = get_messages(f"{wav_folder}/{test_id}.wav")
     nw += nwsjtx
     nt += len(messages)
     pc = nt / nw
