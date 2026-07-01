a710866a3c8853fa04b705e60d57268b14e83f3d
G1OJS <140509227+G1OJS@users.noreply.github.com>
2026-07-01 11:35:05 +0100

    Update PyFT8-like-test using fine tuning and AP.py

--- tests/standalone/PyFT8-like-test using fine tuning and AP.py
+++ tests/standalone/PyFT8-like-test using fine tuning and AP.py
@@ -14,7 +14,7 @@ T_CYC=15
 from PyFT8.receiver import unpack, LdpcDecoder, check_crc
 
 def get_initial_origins(wav_file ):
-    global zgrid_main, HPS, BPT
+
     HPS=4
     BPT=2
     COSTAS = [3,1,4,0,6,5,2]
@@ -26,7 +26,7 @@ def get_initial_origins(wav_file ):
     nFreqs = int(fft_out_len * 2 * max_freq / SAMP_RATE)
     audio_buffer = np.zeros(fft_len, dtype=np.float32)
     fft_in = np.zeros(fft_len, dtype=np.float32)
-    fft_window = fft_window=np.hanning(fft_len).astype(np.float32)
+    fft_window = np.hanning(fft_len).astype(np.float32)
     hops_per_grid = T_CYC * SYM_RATE * HPS
     if (int(hops_per_grid) != hops_per_grid):
         print("Warning - non-integer number of hops per grid")
@@ -59,15 +59,16 @@ def get_initial_origins(wav_file ):
     f_min, f_max = 100, 2900
     for fb in range(int(f_min*BPT/SYM_RATE), nFreqs - 8 * BPT):
         zstrip = zgrid_main[:, fb: fb+8*BPT]
-        dBgrid = 20*np.log10(np.abs(zstrip) + 1e-12)
+        p = 20*np.log10(np.abs(zstrip) + 1e-12)
+        #p = np.abs(zstrip)
 
         origin = {'score':0}
-        for h0_idx in range(0, int(5*HPS/0.16)):
-            sync_score = float(np.dot(dBgrid[h0_idx + BASE_COSTAS_HOPS, :7*BPT].ravel(), csync_flat))
+        for h0_idx in range(int(-2*HPS/0.16), int(5*HPS/0.16)):
+            sync_score = float(np.dot(p[h0_idx + 36*HPS + BASE_COSTAS_HOPS, :7*BPT].ravel(), csync_flat))
             test_sync = {'t0':h0_idx/(HPS * SYM_RATE), 'f0':SYM_RATE * fb / BPT, 'score':sync_score}
             if test_sync['score'] > origin['score']:
                 origin = test_sync
-        if origin['score'] > 2:
+        if origin['score'] > 100:
             origins.append(origin)
 
     return origins
@@ -118,6 +119,7 @@ def get_messages(wav_file):
     for sym_idx, tone in enumerate([3,1,4,0,6,5,2]):
         csync[sym_idx, tone] = 1.0
     csync_flat =  csync.ravel()
+    print(f"{len(origins)} candidates")
     for origin in origins:
         
         ttweaks = np.arange(-50, 51, 10)/1000
@@ -125,7 +127,7 @@ def get_messages(wav_file):
         for ttweak in ttweaks:
             tmp_origin = {'t0':origin['t0']+ttweak, 'f0':origin['f0'], 'score':0}
             zcand = get_candidate_tfgrid(all_audio_spectrum, tmp_origin)
-            score = float(np.dot(np.abs(zcand[:7, :7]).ravel(), csync_flat))
+            score = float(np.dot(np.abs(zcand[36:43, :7]).ravel(), csync_flat))
             scores.append(score)
         ttweak = float(ttweaks[np.argmax(scores)])
         origin = {'t0':origin['t0']+ttweak, 'f0':origin['f0'], 'score':np.max(scores)}
@@ -135,7 +137,7 @@ def get_messages(wav_file):
         for ftweak in ftweaks:
             tmp_origin = {'t0':origin['t0'], 'f0':origin['f0']+ftweak, 'score':0}
             zcand = get_candidate_tfgrid(all_audio_spectrum, tmp_origin)
-            score = float(np.dot(np.abs(zcand[:7, :7]).ravel(), csync_flat))
+            score = float(np.dot(np.abs(zcand[36:43, :7]).ravel(), csync_flat))
             scores.append(score)
         ftweak = float(ftweaks[np.argmax(scores)])
         origin = {'t0':origin['t0'], 'f0':origin['f0']+ftweak, 'score':np.max(scores)}
@@ -178,7 +180,7 @@ def get_messages(wav_file):
             if(bits77_int):
                 msg = unpack(bits77_int)
                 if not msg in messages:
-                    messages[msg] = f"{1+len(messages):03d}:{' '.join(msg):25s} {ipass:03d} {nits:03d}"
+                    messages[msg] = f"{1+len(messages):03d}: {int(0.5+origin['f0']):4d}Hz {origin['t0']-0.5:4.1f}s {' '.join(msg):25s} {ipass:03d} {nits:03d}"
                     print(messages[msg])
     return messages
 
