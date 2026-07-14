4bc454869092e315005a32706019360887c05850
G1OJS <140509227+G1OJS@users.noreply.github.com>
2026-07-14 14:48:48 +0100

    Update spare.py

--- tests/spare.py
+++ tests/spare.py
@@ -1,17 +1,43 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
+import threading, queue, time
+bg = np.random.rand(500,500)
 
-fig = plt.figure( figsize=(8,8) )
+def sender():
+    global msg_box_display_queue, bg
+    x, y = 500*np.random.rand(50), 500*np.random.rand(50)
+    for i in range(50):
+        bg = np.random.rand(500,500)
+        time.sleep(0.01)
+        msg_box_display_queue.put({'x':x[i], 'y':y[i]})
+    
+class Msg_box():
+    def __init__(self, message):
+        from matplotlib.patches import Rectangle
+        self.x, self.y = message['x'], message['y']
+        rect = Rectangle((self.x, self.y), width=40, height=40, alpha=0.6, edgecolor='lime', lw=2)
+        self.patch = ax.add_patch(rect)
+        self.patch.set_visible(True)
 
-a = np.random.rand(500,500)
-im = plt.imshow(a, interpolation='none', aspect='auto', vmin=0, vmax=1)
+fig, ax = plt.subplots( figsize=(8,8) )
+ax.set_xlim(0,500)
+ax.set_ylim(0,500)
 
-def animate_func(i):
-    im.set_data(np.random.rand(500,500))
-    return [im]
 
-anim = animation.FuncAnimation(fig, animate_func, frames = range(100000), interval = 100)
+msg_box_display_queue = queue.Queue()
+img = ax.imshow(bg)
+threading.Thread(target = sender, daemon = True).start()
+plt.pause(0.1)
+#plt.ion()
+while True:
 
-plt.show()  # Not required, it seems!
+    img.set_data(bg)
+
+    while not msg_box_display_queue.empty():
+        message = msg_box_display_queue.get()
+        mb = Msg_box(message)
+        
+    fig.canvas.draw()
+    fig.canvas.flush_events()
+    time.sleep(0.01)
