ca643bf32ba529eadd32a07887fe35ad821bf3cc
G1OJS <140509227+G1OJS@users.noreply.github.com>
2026-07-15 09:32:09 +0100

    Update spare.py

--- tests/spare.py
+++ tests/spare.py
@@ -2,27 +2,34 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import threading, queue, time
-bg = np.random.rand(500,500)
+
+SIZE = 200
+import matplotlib.style as mplstyle
+import matplotlib as mpl
+mplstyle.use('fast')
+
+bg = np.random.rand(SIZE,SIZE)
 
 def sender():
     global msg_box_display_queue, bg
-    x, y = 500*np.random.rand(50), 500*np.random.rand(50)
-    for i in range(50):
-        bg = np.random.rand(500,500)
-        time.sleep(0.01)
+    x, y = SIZE*np.random.rand(50), SIZE*np.random.rand(50)
+    for i in range(20):
+        bg = np.random.rand(SIZE,SIZE)
+        time.sleep(0.16)
         msg_box_display_queue.put({'x':x[i], 'y':y[i]})
     
 class Msg_box():
     def __init__(self, message):
         from matplotlib.patches import Rectangle
         self.x, self.y = message['x'], message['y']
-        rect = Rectangle((self.x, self.y), width=40, height=40, alpha=0.6, edgecolor='lime', lw=2)
+        rect = Rectangle((self.x, self.y), width=100, height=6, alpha=0.6, edgecolor='lime', lw=2)
         self.patch = ax.add_patch(rect)
         self.patch.set_visible(True)
-
+        self.text_inst = ax.text(self.x, self.y, 'Testing text', fontsize='small', fontweight='bold', clip_on = True )
+ 
 fig, ax = plt.subplots( figsize=(8,8) )
-ax.set_xlim(0,500)
-ax.set_ylim(0,500)
+ax.set_xlim(0,SIZE)
+ax.set_ylim(0,SIZE)
 
 
 msg_box_display_queue = queue.Queue()
@@ -37,7 +44,9 @@ while True:
     while not msg_box_display_queue.empty():
         message = msg_box_display_queue.get()
         mb = Msg_box(message)
-        
+
+    t = time.time()
     fig.canvas.draw()
+    print(time.time() - t)
     fig.canvas.flush_events()
     time.sleep(0.01)
