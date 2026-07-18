c1774b6fd2d043a9c2e604bc081e69c904ae9afa
G1OJS <140509227+G1OJS@users.noreply.github.com>
2026-07-18 20:24:40 +0100

    live playback tests

--- tests/plot_decode_times.py
+++ tests/plot_decode_times.py
@@ -13,13 +13,11 @@ i0, i1 = 1, 39
     
 fig, ax = plt.subplots(figsize = (9,9))
 
-plot_from_file(ax, 'live_decode_times_PyFT8_1.pkl', 'PyFT8_1', 'limegreen')
-plot_from_file(ax, 'live_decode_times_PyFT8_2.pkl', 'PyFT8_2', 'limegreen')
-#plot_from_file(ax, 'live_decode_times_PyFT8_3.pkl', 'PyFT8_3', 'limegreen')
+plot_from_file(ax, 'live_decode_times_PyFT8.pkl', 'PyFT8', 'limegreen')
 
 plot_from_file(ax, 'live_decode_times_WSJTx_2.7.0_1_FAST.pkl', 'WSJT-x_2.7.0_FAST', 'grey')
 plot_from_file(ax, 'live_decode_times_WSJTx_2.7.0_2_NORM.pkl', 'WSJT-x_2.7.0_NORM', 'purple')
-#plot_from_file(ax, 'live_decode_times_WSJTx_2.7.0_3_DEEP.pkl', 'WSJT-x_2.7.0_NORM', 'black')
+plot_from_file(ax, 'live_decode_times_WSJTx_2.7.0_3_DEEP.pkl', 'WSJT-x_2.7.0_DEEP', 'black')
         
 ax.set_xlabel("Time, seconds")
 ax.set_ylabel("Cumulative decodes (unique per cycle)")
