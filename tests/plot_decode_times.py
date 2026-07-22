59bad75ec7e8a017c89e5d5fc6563640fe9720a9
G1OJS <140509227+G1OJS@users.noreply.github.com>
2026-07-22 17:05:05 +0100

    test results

--- tests/plot_decode_times.py
+++ tests/plot_decode_times.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.ticker import AutoMinorLocator, MultipleLocator
 import pickle
 
 
@@ -12,24 +13,29 @@ def plot_from_file(ax, fname, label, color):
 i0, i1 = 1, 39
     
 fig, ax = plt.subplots(figsize = (9,9))
+ax.yaxis.set_label_position("right")
 
-plot_from_file(ax, 'live_decode_times_PyFT8_4_8_28.pkl', 'PyFT8', 'limegreen')
 
-plot_from_file(ax, 'live_decode_times_WSJTx_2_8_28_FAST.pkl', 'WSJT-x_2.7.0_FAST', 'black')
-plot_from_file(ax, 'live_decode_times_WSJTx_5_8_28_FAST-AP.pkl', 'WSJT-x_2.7.0_FAST-AP', 'blue')
 
-plot_from_file(ax, 'live_decode_times_WSJTx_3_8_28_DEEP.pkl', 'WSJT-x_2.7.0_DEEP', 'purple')
-#plot_from_file(ax, 'live_decode_times_WSJTx_4_8_28_DEEP-AP.pkl', 'WSJT-x_2.7.0_DEEP-AP', 'red')
+plot_from_file(ax, 'live_decode_times_PyFT8_1_8_28.pkl', 'PyFT8', 'limegreen')
+
+plot_from_file(ax, 'live_decode_times_WSJTx_1_8_28_FAST.pkl', 'WSJT-x_2.7.0_FAST', 'black')
+plot_from_file(ax, 'live_decode_times_WSJTx_2_8_28_DEEP.pkl', 'WSJT-x_2.7.0_DEEP', 'blue')
+
+ax.legend()
         
 ax.set_xlabel("Time, seconds")
 ax.set_ylabel("Cumulative decodes (unique per cycle)")
-fig.suptitle("Cumulative decode count against time")
+fig.suptitle("Cumulative decode count against time\nPyFT8 vs WSJT-x v2.7.0")
 ax.legend()
 
 from matplotlib.ticker import AutoMinorLocator, MultipleLocator
 ax.xaxis.set_major_locator(MultipleLocator(15))
 ax.xaxis.set_minor_locator(MultipleLocator(1))
+ax.yaxis.set_major_locator(MultipleLocator(100))
+ax.yaxis.set_minor_locator(MultipleLocator(25))
 plt.grid(which = 'major', axis = 'x')
 plt.grid(which = 'major', axis = 'y')
+ax.yaxis.tick_right()
 
 plt.show()
