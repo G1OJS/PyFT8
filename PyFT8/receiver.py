9db9e4a5eb7343e99a7beec092ea3586e2a3d8ba
G1OJS <140509227+G1OJS@users.noreply.github.com>
2026-07-25 10:06:10 +0100

    move decode and unpack to decoders.py

--- PyFT8/receiver.py
+++ PyFT8/receiver.py
@@ -1,9 +1,8 @@
 import threading
 import numpy as np
 import pyaudio
-
 from PyFT8.time_utils import time_utils
-from PyFT8.databases import call_hashes, add_call_hashes
+from PyFT8.decoders import ldpc_decode, osd_decode
 
 WATERFALL_DOWNSAMPLE = 2
 DEBUG_PRINTS = True
@@ -15,235 +14,16 @@ COSTAS = [3,1,4,0,6,5,2]
 PAYLOAD_SYMB_IDXS = list(range(7, 36)) + list(range(43, 72))
 COSTAS_SYMB_IDXS = list(range(7)) + list(range(36,43)) + list(range(72,79))
 
-#=========== Unpacking functions ========================================
-def get_bitfields(bits, lengths):
-    fields = []
-    for n in lengths:
-        mask = (1 << n) - 1
-        fields.append(bits & mask)
-        bits >>= n
-    return *fields, bits
-
-def unpack(bits):
-    if not bits:
-        return None
-    
-    i3, bits74 = get_bitfields(bits,[3])
-    if i3 == 0:
-        n3, bits71 = get_bitfields(bits74,[3])
-        if n3 <= 4:
-            return (['Free text', 'DXpedition','Field Day', 'Field Day', 'Telemetry'][n3],'not','implemented')
-        else:
-            return ('Unknown mode','not','implemented')
-    elif i3 == 1 or i3 == 2: # 1 = Std Msg incl /R 2 = 'EU VHF' = Std Msg incl /P
-        return unpack_std(bits74, i3)
-    elif i3 == 3:
-        return ('RTTY RU','not','implemented')
-    elif i3 == 4:
-        cq, rrr, swp, c58, hsh, _ = get_bitfields(bits74, [1,2,1,58,12]) 
-        ca = "CQ" if cq else call_hashes.get((hsh,12), '<....>')
-        cb = ""
-        for i in range(12):
-            cb = " 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ/"[c58 % 38] + cb
-            c58 = c58 // 38
-        cb =  cb.strip()
-        add_call_hashes(cb)
-        (ca, cb) = (cb, ca) if swp else (ca, cb)
-        return (ca, cb, ('', 'RRR', 'RR73', '73')[rrr])
-    elif i3 == 5:
-        return ('EU VHF','not','implemented')
-
-def unpack_std(bits74, i3):
-    g16, cb29, ca29, _ = get_bitfields(bits74,[16,29,29])
-    g15 = g16 & 0x7FFF
-    if g15 < 32400:
-        a, nn = divmod(g15, 1800)
-        b, nn = divmod(nn, 100)
-        c, d = divmod(nn, 10)
-        grid_rpt =  chr(65+a) + chr(65+b) + str(c) + str(d)
-    elif g15 - 32400 <= 4:
-        grid_rpt =  ('', '', 'RRR', 'RR73', '73')[g15 - 32400]
-    else:
-        prefix = 'R' if (g16 >> 15) else ''
-        grid_rpt = prefix + f"{(g15 - 32435):+03d}"
-    msg_tuple = (call_29(ca29, i3), call_29(cb29, i3), grid_rpt)
-    if msg_tuple != ('','','') and msg_tuple is not None:
-        return msg_tuple
-
-def call_29(call_int29, i3):    
-    portable_rover = call_int29 & 1
-    call_int28 = call_int29>>1
-    if call_int28 < 3:
-        return ['DE', 'QRZ', 'CQ'][call_int28]
-    elif call_int28 < 1004:
-        return f"CQ {call_int28 - 3:03d}"
-    elif call_int28 < 21443:
-        x, txt = call_int28 - 1003, ''
-        for i in range(4):
-            txt = " ABCDEFGHIJKLMNOPQRSTUVWXYZ"[int(x % 27)] + txt
-            x //= 27
-        return f"CQ {txt.strip()}"
-    elif call_int28 < 2063592+4194303:
-        return call_hashes.get((call_int28 - 2063592, 22), '<....>')
-    else:
-        call = standard_call28(call_int28, i3)
-        if portable_rover:
-            call = call + ('/P' if i3 == 2 else '/R')
-        add_call_hashes(call)
-        return call
-
-def standard_call28(call_int28, i3):
-    nn = call_int28 - (2063592 + 4194304)
-    from string import ascii_uppercase as ltrs, digits as digs
-    call_fields = [ (' ' + digs + ltrs, 36*10*27**3),   (digs + ltrs, 10*27**3), (digs + ' ' * 17, 27**3),
-                    (' ' + ltrs, 27**2),           (' ' + ltrs,   27), (' ' + ltrs,   1) ]
-    chars = []
-    for alphabet, div in call_fields:
-        idx, nn = divmod(nn, div)
-        chars.append(alphabet[idx])
-    call = ''.join(chars).strip()
-    return call
-
-def crc_unpack(codeword91):
-    bits91_int = 0
-    for bit in (codeword91 > 0).astype(int).tolist():
-        bits91_int = (bits91_int << 1) | bit
-    bits77_int = bits91_int >> 14
-    if(bits77_int > 0):
-        crc14_int = 0
-        for i in range(96):
-            inbit = ((bits77_int >> (76 - i)) & 1) if i < 77 else 0
-            bit14 = (crc14_int >> (14 - 1)) & 1
-            crc14_int = ((crc14_int << 1) & ((1 << 14) - 1)) | inbit
-            if bit14:
-                crc14_int ^= 0x2757
-        if(crc14_int == bits91_int & 0b11111111111111):
-            return unpack(bits77_int)
-
-#============== LDPC ===========================================================
-CV6idx = np.array([[4,31,59,92,114,145],[5,23,60,93,121,150],[6,32,61,94,95,142],[5,31,63,96,125,137],[8,34,65,98,138,145],[9,35,66,99,106,125],[11,37,67,101,104,154],[12,38,68,102,148,161],[14,41,58,105,122,158],[0,32,71,105,106,156],[15,42,72,107,140,159],[10,43,74,109,120,165],[7,45,70,111,118,165],[18,37,76,103,115,162],[19,46,69,91,137,164],[1,47,73,112,127,159],[21,46,57,117,126,163],[15,38,61,111,133,157],[22,42,78,119,130,144],[19,35,62,93,135,160],[13,30,78,97,131,163],[2,43,79,123,126,168],[18,45,80,116,134,166],[11,49,60,117,118,143],[12,50,63,113,117,156],[23,51,75,128,147,148],[20,53,76,99,139,170],[34,81,132,141,170,173],[13,29,82,112,124,169],[3,28,67,119,133,172],[51,83,109,114,144,167],[6,49,80,98,131,172],[22,54,66,94,171,173],[25,40,76,108,140,147],[26,39,55,123,124,125],[17,48,54,123,140,166],[5,32,84,107,115,155],[8,53,62,130,146,154],[21,52,67,108,120,173],[2,12,47,77,94,122],[30,68,132,149,154,168],[4,38,74,101,135,166],[1,53,85,100,134,163],[14,55,86,107,118,170],[22,33,70,93,126,152],[10,48,87,91,141,156],[28,33,86,96,146,161],[21,56,84,92,139,158],[27,31,71,102,131,165],[0,25,44,79,127,146],[16,26,88,102,115,152],[50,56,97,162,164,171],[20,36,72,137,151,168],[15,46,75,129,136,153],[2,23,29,71,103,138],[8,39,89,105,133,150],[17,41,78,143,145,151],[24,37,64,98,121,159],[16,41,74,128,169,171]], dtype = np.int16)
-CV7idx = np.array([[3,30,58,90,91,95,152],[7,24,62,82,92,95,147],[4,33,64,77,97,106,153],[10,36,66,86,100,138,157],[7,39,69,81,103,113,144],[13,40,70,87,101,122,155],[16,36,73,80,108,130,153],[44,54,63,110,129,160,172],[17,35,75,88,112,113,142],[20,44,77,82,116,120,150],[18,34,58,72,109,124,160],[6,48,57,89,99,104,167],[24,52,68,89,100,129,155],[19,45,64,79,119,139,169],[0,3,51,56,85,135,151],[25,50,55,90,121,136,167],[1,26,40,60,61,114,132],[27,47,69,84,104,128,157],[11,42,65,88,96,134,158],[9,43,81,90,110,143,148],[29,49,59,85,136,141,161],[9,52,65,83,111,127,164],[27,28,83,87,116,142,149],[14,57,59,73,110,149,162]], dtype = np.int16)
-
-import warnings
-warnings.filterwarnings("error")
-
-def pass_ldpc_messages(llr, CVidx, mC2V_prev, update_collector):
-    mV2C = llr[CVidx] - mC2V_prev
-    tanh_mV2C = np.tanh(-mV2C)
-    tanh_mC2V = np.prod(tanh_mV2C, axis=1, keepdims=True)
-    orig_err = np.geterr()
-    np.seterr(all = 'ignore')
-    tanh_mC2V = np.divide(tanh_mC2V, tanh_mV2C)
-    np.seterr(**orig_err)
-    alpha_atanh_approx = 1.18
-    mC2V_curr  = tanh_mC2V / ((tanh_mC2V - alpha_atanh_approx) * (alpha_atanh_approx + tanh_mC2V))
-    np.add.at(update_collector, CVidx, mC2V_curr - mC2V_prev)
-    return mC2V_curr
-
-def ldpc_decode(llr, max_ncheck0, max_iters):
-    mC2V_prev6, mC2V_prev7 = np.zeros(CV6idx.shape, dtype=np.float32), np.zeros(CV7idx.shape, dtype=np.float32)
-    for n_its in range(max_iters):
-        bits6, bits7 = llr[CV6idx] > 0, llr[CV7idx] > 0
-        parity6, parity7 = np.sum(bits6, axis=1) & 1, np.sum(bits7, axis=1) & 1
-        ncheck = int(np.sum(parity7) + np.sum(parity6))
-        if n_its == 0 and ncheck > max_ncheck0:
-            return None, -1, []
-        if ncheck == 0:
-            msg_tuple = crc_unpack(llr[:91])
-            if msg_tuple:
-                return msg_tuple, n_its, []
-        else:
-            update_collector = np.zeros_like(llr)
-            mC2V_prev6 = pass_ldpc_messages(llr, CV6idx, mC2V_prev6, update_collector)
-            mC2V_prev7 = pass_ldpc_messages(llr, CV7idx, mC2V_prev7, update_collector)
-            llr += update_collector
-
-    return None, -1, llr
-
-
-#============== OSD ===========================================================
-from itertools import combinations
-generator_matrix_rows = ["8329ce11bf31eaf509f27fc",  "761c264e25c259335493132",  "dc265902fb277c6410a1bdc",  "1b3f417858cd2dd33ec7f62",  "09fda4fee04195fd034783a",  "077cccc11b8873ed5c3d48a",  "29b62afe3ca036f4fe1a9da",  "6054faf5f35d96d3b0c8c3e",  "e20798e4310eed27884ae90",  "775c9c08e80e26ddae56318",  "b0b811028c2bf997213487c",  "18a0c9231fc60adf5c5ea32",  "76471e8302a0721e01b12b8",  "ffbccb80ca8341fafb47b2e",  "66a72a158f9325a2bf67170",  "c4243689fe85b1c51363a18",  "0dff739414d1a1b34b1c270",  "15b48830636c8b99894972e",  "29a89c0d3de81d665489b0e",  "4f126f37fa51cbe61bd6b94",  "99c47239d0d97d3c84e0940",  "1919b75119765621bb4f1e8",  "09db12d731faee0b86df6b8",  "488fc33df43fbdeea4eafb4",  "827423ee40b675f756eb5fe",  "abe197c484cb74757144a9a",  "2b500e4bc0ec5a6d2bdbdd0",  "c474aa53d70218761669360",  "8eba1a13db3390bd6718cec",  "753844673a27782cc42012e",  "06ff83a145c37035a5c1268",  "3b37417858cc2dd33ec3f62",  "9a4a5a28ee17ca9c324842c",  "bc29f465309c977e89610a4",  "2663ae6ddf8b5ce2bb29488",  "46f231efe457034c1814418",  "3fb2ce85abe9b0c72e06fbe",  "de87481f282c153971a0a2e",  "fcd7ccf23c69fa99bba1412",  "f0261447e9490ca8e474cec",  "4410115818196f95cdd7012",  "088fc31df4bfbde2a4eafb4",  "b8fef1b6307729fb0a078c0",  "5afea7acccb77bbc9d99a90",  "49a7016ac653f65ecdc9076",  "1944d085be4e7da8d6cc7d0",  "251f62adc4032f0ee714002",  "56471f8702a0721e00b12b8",  "2b8e4923f2dd51e2d537fa0",  "6b550a40a66f4755de95c26",  "a18ad28d4e27fe92a4f6c84",  "10c2e586388cb82a3d80758",  "ef34a41817ee02133db2eb0",  "7e9c0c54325a9c15836e000",  "3693e572d1fde4cdf079e86",  "bfb2cec5abe1b0c72e07fbe",  "7ee18230c583cccc57d4b08",  "a066cb2fedafc9f52664126",  "bb23725abc47cc5f4cc4cd2",  "ded9dba3bee40c59b5609b4",  "d9a7016ac653e6decdc9036",  "9ad46aed5f707f280ab5fc4",  "e5921c77822587316d7d3c2",  "4f14da8242a8b86dca73352",  "8b8b507ad467d4441df770e",  "22831c9cf1169467ad04b68",  "213b838fe2ae54c38ee7180",  "5d926b6dd71f085181a4e12",  "66ab79d4b29ee6e69509e56",  "958148682d748a38dd68baa",  "b8ce020cf069c32a723ab14",  "f4331d6d461607e95752746",  "6da23ba424b9596133cf9c8",  "a636bcbc7b30c5fbeae67fe",  "5cb0d86a07df654a9089a20",  "f11f106848780fc9ecdd80a",  "1fbb5364fb8d2c9d730d5ba",  "fcb86bc70a50c9d02a5d034",  "a534433029eac15f322e34c",  "c989d9c7c3d3b8c55d75130",  "7bb38b2f0186d46643ae962",  "2644ebadeb44b9467d1f42c",  "608cc857594bfbb55d69600"]
-kGEN = np.array([int(row,16)>>1 for row in generator_matrix_rows])
-A = np.zeros((83, 91), dtype=np.uint8)
-for i, row in enumerate(kGEN):
-    for j in range(91):
-        A[i, 90 - j] = (row >> j) & 1
-G = np.concatenate([np.eye(91, dtype=np.uint8), A.T],axis=1)
-
-def gf2_systematic_from_reliability(G, reliab_order):
-    G = (G.copy() & 1).astype(np.uint8)
-    k, n = G.shape
-    colperm = np.array(reliab_order, dtype=np.int64)  
-    inv = np.empty(n, dtype=np.int64)
-    inv[colperm] = np.arange(n)
-    G = G[:, colperm] 
-    # Gauss-Jordan:
-    row = 0
-    for col in range(n):
-        if row >= k:
-            break
-        pivot_rows = np.where(G[row:, col] == 1)[0]
-        if pivot_rows.size == 0:
-            continue
-        piv = row + pivot_rows[0]
-        if piv != row:
-            G[[row, piv], :] = G[[piv, row], :]
-        ones = np.where(G[:, col] == 1)[0]
-        for r in ones:
-            if r != row:
-                G[r, :] ^= G[row, :]
-        if col != row:
-            G[:, [row, col]] = G[:, [col, row]]
-            colperm[[row, col]] = colperm[[col, row]]
-        row += 1
-    if row < k:
-        raise ValueError("Could not find k independent columns to form a systematic generator.")
-    return G, colperm
-
-def encode_and_score(u, Gsys, bits_sys, vals_sys):
-    u = (u.astype(np.uint8) & 1)
-    codeword = (u @ Gsys) & 1
-    bit_diff = codeword ^ bits_sys
-    score = float(np.sum(vals_sys * bit_diff))
-    return codeword, score
-
-def osd_decode(llr, reliab_order, Ls = [55,10]):
-
-    # determine colperm ordering and inverse
-    Gsys, colperm = gf2_systematic_from_reliability(G, reliab_order)
-    colperm_inv = np.empty(174, dtype=np.int64)
-    colperm_inv[colperm] = np.arange(174)
-
-    # permute bits and strengths to new order
-    llr_sys = llr.astype(np.float32)[colperm]
-    bits_sys, vals_sys = (llr_sys > 0).astype(np.uint8), np.abs(llr_sys).astype(np.float32)
-    bits_sys91 = bits_sys[:91]
-    rel_ord91 = np.argsort(vals_sys[:91])
-
-    best = encode_and_score(bits_sys91, Gsys, bits_sys, vals_sys)
-    for t in range(1, len(Ls) + 1):
-        flip_pool = rel_ord91[:min(Ls[t-1], 91)]
-        for comb in combinations(flip_pool, t):
-            bits91_test = bits_sys91.copy()
-            bits91_test[list(comb)] ^= 1
-            test = encode_and_score(bits91_test, Gsys, bits_sys, vals_sys)
-            if test[1] < best[1]:
-                best = test
-            
-    cw = best[0][colperm_inv].astype(np.uint8)
-    msg_tuple = crc_unpack(cw[:91])
-    return msg_tuple
 
 
 #============== CANDIDATE ===========================================================
 
 ap_patterns = [
-                ['None',0, []],                                                                # no AP
-                ['CQ',0, [0,0,0,0,0 ,0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,1,0,0]],  # CQ
-                ['RR73',58,[0,1, 1,1,1,1,1, 0,0,1,1,1, 0,1,0,1,0, 0,1]],                       # RR73
-                ['73',58,[0,1, 1,1,1,1,1, 0,1,0,0,1, 0,1,0,0,0, 0,1]],                       # 73
-                ['RRR',58,[0,1, 1,1,1,1,1, 0,1,0,0,1, 0,0,1,0,0, 0,1]],                       # RRR
+                ['None',     0, []                                                               ], 
+                ['CQ',       0, [0,0,0,0,0 ,0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,1,0,0] ],
+                ['RR73',    58, [0,1, 1,1,1,1,1, 0,0,1,1,1, 0,1,0,1,0, 0,1]                      ],
+                ['73',      58, [0,1, 1,1,1,1,1, 0,1,0,0,1, 0,1,0,0,0, 0,1]                      ],
+                ['RRR',     58, [0,1, 1,1,1,1,1, 0,1,0,0,1, 0,0,1,0,0, 0,1]                      ],
               ]
 
 class Candidate:
@@ -401,6 +181,7 @@ class Candidate:
         if msg_tuple:
             self.decode_status = f'{source} OSD {pat_name}'
             self.msg_tuple, self.n_its = msg_tuple, -1
+
 
 #============== AUDIO IN ===========================================================
 class AudioIn:
@@ -603,7 +384,7 @@ class Receiver():
             if len(to_decode):
                 to_decode.sort(key=lambda c: c.llr_sd, reverse=True)
                 max_ipass = 1 + np.min([c.ipass for c in to_decode])
-                for c in to_decode[:40]:
+                for c in to_decode:
                     c.decode(max_ipass)
             else:
                 if not end_decoding_message_printed and len([c for c in self.candidates if c.demap_started]):
