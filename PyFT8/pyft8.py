493154e352e0a17861dab6b4c3480e3968373b96
G1OJS <140509227+G1OJS@users.noreply.github.com>
2026-08-06 15:40:44 +0100

    version

--- PyFT8/pyft8.py
+++ PyFT8/pyft8.py
@@ -10,7 +10,7 @@ from PyFT8.databases import History
 from PyFT8.qso_manager import QSO_manager
 from PyFT8.databases import History
 
-VER = '3.7.5'
+VER = '3.7.6'
 PSKR_REFRESH_MINS = 20
 
 def get_config(config_folder):
